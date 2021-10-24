95026df5f385d00496dc969ca12fd8a63a7701e7
Florian Trayon <git@florian-trayon.fr>
2021-10-24 21:58:46 +0200

    Added a new constant to change the name of the storage folder

--- constants.inc.php
+++ constants.inc.php
@@ -5,19 +5,22 @@
 	// Mot de passe pour utiliser le service.
 	const PASSWORD = "password";
 
+	// Nom du répertoire de sauvegarde des fichiers.
+	const STORAGE_FOLDER = "public";
+
 	// Taille maximale des fichiers.
 	// Note : ne doit pas dépasser le paramètre suivant : https://www.php.net/manual/fr/ini.core.php#ini.upload-max-filesize
 	const MAX_SIZE = 2097152;
 
 	// Extensions et types MIME autorisés.
-	const EXTENSIONS = array(
+	const EXTENSIONS = [
 		"jpg" => "image/jpeg", "jpeg" => "image/jpeg",
 		"png" => "image/png",
 		"gif" => "image/gif",
-	);
+	];
 
 	// Messages d'erreurs compréhensible par l'utilisateur final.
-	const ERROR_MESSAGES = array(
+	const ERROR_MESSAGES = [
 		0 => "Il n'y a pas d'erreur, le fichier a été téléchargé avec succès.",
 		1 => "Le poids du fichier téléchargé dépasse la directive « upload_max_filesize » de la configuration php.ini.",
 		2 => "Le poids du fichier téléchargé dépasse la directive « MAX_FILE_SIZE » spécifié dans le formulaire HTML.",
@@ -26,5 +29,5 @@
 		6 => "Le dossier temporaire est introuvable.",
 		7 => "Impossible d'écrire le fichier sur le disque.",
 		8 => "Une extension PHP a arrêté le téléchargement du fichier.",
-	);
+	];
 ?>
