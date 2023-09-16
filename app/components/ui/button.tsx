5ae5d6157ab67644a1ff4f6f5c2b1180ffb3f6d8
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-09-16 16:31:14 +0200

    Fixed ESLint warnings on UI Components provided by shadcn

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,15 +1,18 @@
-import * as React from "react";
+//
+// Composant générique des boutons.
+//  Source : https://ui.shadcn.com/docs/components/button
+//
 import { Slot } from "@radix-ui/react-slot";
+import { merge } from "@/utilities/tailwind";
 import { cva, type VariantProps } from "class-variance-authority";
-
-import { cn } from "@/utilities/tailwind";
+import { forwardRef, type ButtonHTMLAttributes } from "react";
 
 const buttonVariants = cva(
 	"inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
 	{
 		variants: {
 			variant: {
-				default: "bg-primary text-primary-foreground hover:bg-primary/90",
+				default: "bg-primary hover:bg-primary/90",
 				destructive:
 					"bg-destructive text-destructive-foreground hover:bg-destructive/90",
 				outline:
@@ -34,25 +37,29 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-	VariantProps<typeof buttonVariants>
-{
+	extends ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {
 	asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 	( { className, variant, size, asChild = false, ...props }, ref ) =>
 	{
 		const Comp = asChild ? Slot : "button";
+
 		return (
 			<Comp
-				className={cn( buttonVariants( { variant, size, className } ) )}
 				ref={ref}
+				className={merge( buttonVariants( { variant, size, className } ) )}
 				{...props}
 			/>
 		);
 	}
 );
+
+Button.defaultProps = {
+	asChild: false
+};
 
 Button.displayName = "Button";
 
--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,26 +1,26 @@
-import * as React from "react";
+//
+// Composant générique des champs de saisie.
+//  Source : https://ui.shadcn.com/docs/components/input
+//
+import { merge } from "@/utilities/tailwind";
+import { forwardRef, type InputHTMLAttributes } from "react";
 
-import { cn } from "@/utilities/tailwind";
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export interface InputProps
-	extends React.InputHTMLAttributes<HTMLInputElement> { }
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-	( { className, type, ...props }, ref ) =>
-	{
-		return (
-			<input
-				type={type}
-				className={cn(
-					"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-					className
-				)}
-				ref={ref}
-				{...props}
-			/>
-		);
-	}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	( { className, type, ...props }, ref ) => (
+		<input
+			ref={ref}
+			type={type}
+			className={merge(
+				"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+				className
+			)}
+			{...props}
+		/>
+	)
 );
+
 Input.displayName = "Input";
 
 export { Input };
