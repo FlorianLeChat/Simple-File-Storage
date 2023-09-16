2f48050d4acc41c356c2d38495dc492e3f0e63ba
Florian Trayon <git@florian-trayon.fr>
2023-09-16 16:31:14 +0200

    Fixed ESLint warnings on UI Components provided by shadcn

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -1,26 +1,31 @@
-"use client";
-
-import * as React from "react";
+//
+// Composant générique des libellés.
+//  Source : https://ui.shadcn.com/docs/components/label
+//
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
+import { forwardRef,
+	type ElementRef,
+	type ComponentPropsWithoutRef } from "react";
 
-import { cn } from "@/utilities/tailwind";
+import { merge } from "@/utilities/tailwind";
 
 const labelVariants = cva(
 	"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
-const Label = React.forwardRef<
-	React.ElementRef<typeof LabelPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-	VariantProps<typeof labelVariants>
+const Label = forwardRef<
+	ElementRef<typeof LabelPrimitive.Root>,
+	ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+		VariantProps<typeof labelVariants>
 >( ( { className, ...props }, ref ) => (
 	<LabelPrimitive.Root
 		ref={ref}
-		className={cn( labelVariants(), className )}
-		{...props}
+		className={merge( labelVariants(), className )}
+		{...{ props }}
 	/>
 ) );
+
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
