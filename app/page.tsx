e8b475682edc5e9455c79d1b220460efd44281f5
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-09-18 18:09:17 +0200

    Added an animated icon to the GitHub repository

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@
 //
 
 // Importation des dépendances.
-import Link from "next/link";
 import { lazy } from "react";
-import { merge } from "@/utilities/tailwind";
-
-// Importation des composants.
-import { buttonVariants } from "./components/ui/button";
 
 const AuthForm = lazy( () => import( "./components/auth-form" ) );
 
@@ -25,16 +20,33 @@ export default function Page()
 					💾 Simple File Storage
 				</h1>
 
-				{/* Redirection vers la connexion */}
-				<Link
-					href="/authentication"
-					className={merge(
-						buttonVariants( { variant: "outline" } ),
-						"absolute right-4 top-4 hover:bg-accent hover:text-accent-foreground"
-					)}
+				{/* Affichage de l'animation du logo vers le dépôt GitHub */}
+				{/* Source : https://tholman.com/github-corners/ */}
+				<a
+					rel="noopener noreferrer"
+					href="https://github.com/FlorianLeChat/Simple-File-Storage"
+					target="_blank"
+					className="group fixed bottom-auto left-auto right-0 top-0 hidden sm:inline"
 				>
-					Connexion
-				</Link>
+					<svg
+						width="80"
+						height="80"
+						viewBox="0 0 250 250"
+						className="fill-primary text-background"
+					>
+						<path d="M0 0l115 115h15l12 27 108 108V0z" />
+						<path
+							d="M128 109c-15-9-9-19-9-19 3-7 2-11 2-11-1-7 3-2 3-2 4 5 2 11 2 11-3 10 5 15 9 16"
+							className="md:motion-safe:animate-github group-hover:md:motion-safe:animate-github origin-[130px_106px] fill-current"
+						/>
+						<path
+							d="M115 115s4 2 5 0l14-14c3-2 6-3 8-3-8-11-15-24 2-41 5-5 10-7 16-7 1-2 3-7 12-11 0 0
+							5 3 7 16 4 2 8 5 12 9s7 8 9 12c14 3 17 7 17 7-4 8-9 11-11 11 0 6-2 11-7 16-16 16-30 10-41
+							2 0 3-1 7-5 11l-12 11c-1 1 1 5 1 5z"
+							className="fill-current"
+						/>
+					</svg>
+				</a>
 			</header>
 
 			{/* Vidéo en arrière-plan */}
