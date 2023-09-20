39d0bb4cf1dc87a54443ac3d8169521c494effe8
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-09-20 17:14:27 +0200

    Added (again) new shadcn components for authentication page

---
+++ app/components/ui/toaster.tsx
@@ -0,0 +1,41 @@
+//
+// Composant d'affichage des notifications.
+//  Source : https://ui.shadmerge.com/docs/components/toast
+//
+
+"use client";
+
+import { useToast } from "@/app/components/ui/use-toast";
+import { Toast,
+	ToastClose,
+	ToastTitle,
+	ToastViewport,
+	ToastProvider,
+	ToastDescription } from "@/app/components/ui/toast";
+
+export function Toaster()
+{
+	const { toasts } = useToast();
+
+	return (
+		<ToastProvider>
+			{toasts.map( ( { id, title, description, action, ...props } ) => (
+				<Toast key={id} {...props}>
+					<div className="grid gap-1">
+						{title && <ToastTitle>{title}</ToastTitle>}
+
+						{description && (
+							<ToastDescription>{description}</ToastDescription>
+						)}
+					</div>
+
+					{action}
+
+					<ToastClose />
+				</Toast>
+			) )}
+
+			<ToastViewport />
+		</ToastProvider>
+	);
+}
