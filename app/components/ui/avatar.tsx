af20e178a4d7be88c728ceb4aedb07f2b55db5bd
Florian Trayon <git@florian-trayon.fr>
2023-09-21 19:58:53 +0200

    Added several shadcn components for all settings pages

---
+++ app/components/ui/avatar.tsx
@@ -0,0 +1,59 @@
+//
+// Composant générique de l'avatar.
+//  Source : https://ui.shadcn.com/docs/components/avatar
+//
+
+"use client";
+
+import { merge } from "@/utilities/tailwind";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { forwardRef,
+	type ElementRef,
+	type ComponentPropsWithoutRef } from "react";
+
+const Avatar = forwardRef<
+	ElementRef<typeof AvatarPrimitive.Root>,
+	ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>( ( { className, ...props }, ref ) => (
+	<AvatarPrimitive.Root
+		ref={ref}
+		className={merge(
+			"relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+			className
+		)}
+		{...props}
+	/>
+) );
+
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = forwardRef<
+	ElementRef<typeof AvatarPrimitive.Image>,
+	ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>( ( { className, ...props }, ref ) => (
+	<AvatarPrimitive.Image
+		ref={ref}
+		className={merge( "aspect-square h-full w-full", className )}
+		{...props}
+	/>
+) );
+
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = forwardRef<
+	ElementRef<typeof AvatarPrimitive.Fallback>,
+	ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>( ( { className, ...props }, ref ) => (
+	<AvatarPrimitive.Fallback
+		ref={ref}
+		className={merge(
+			"flex h-full w-full items-center justify-center rounded-full bg-muted",
+			className
+		)}
+		{...props}
+	/>
+) );
+
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
