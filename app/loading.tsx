f01da5c2a29545dfcc203fdcedb2f5a689dcc598
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-09-21 19:53:25 +0200

    Fixed client-side component loading screen layout

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,16 +2,15 @@
 // Page de chargement des composants asynchrones.
 //  Source : https://nextjs.org/docs/app/api-reference/file-conventions/loading
 //
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Loader2 } from "lucide-react";
 
-export default function Loading( { title }: { title: string } )
+export default function Loading()
 {
 	// Affichage du rendu HTML du composant.
 	return (
 		<div className="flex h-screen flex-col items-center justify-center gap-4 text-4xl font-bold uppercase">
-			💾 {title}
-			<FontAwesomeIcon spin icon={faSpinner} />
+			💾 Simple File Storage
+			<Loader2 className="h-14 w-14 animate-spin" />
 		</div>
 	);
 }
