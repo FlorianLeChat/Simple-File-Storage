c9dace06054d72fd1f341e43971bf13a5f13b10a
Florian Trayon <git@florian-trayon.fr>
2023-09-23 18:35:06 +0200

    Fixed generation errors in website layout settings page

--- app/settings/components/layout.tsx
+++ app/settings/components/layout.tsx
@@ -1,73 +1,88 @@
+//
+// Composant de paramétrage de l'apparence du site.
+//
+
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { merge } from "@/utilities/tailwind";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChevronDown, Loader2, RefreshCw } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/registry/new-york/ui/button";
+import { toast } from "../../components/ui/use-toast";
 import { Form,
 	FormControl,
 	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
-	FormMessage } from "@/registry/new-york/ui/form";
-import { RadioGroup, RadioGroupItem } from "@/registry/new-york/ui/radio-group";
-import { toast } from "@/registry/new-york/ui/use-toast";
+	FormMessage } from "../../components/ui/form";
+import { Button, buttonVariants } from "../../components/ui/button";
+import { RadioGroup, RadioGroupItem } from "../../components/ui/radio-group";
 
-const appearanceFormSchema = z.object( {
-	theme: z.enum( [ "light", "dark" ], {
-		required_error: "Please select a theme."
-	} ),
-	font: z.enum( [ "inter", "manrope", "system" ], {
-		invalid_type_error: "Select a font",
-		required_error: "Please select a font."
-	} )
+// Déclaration du schéma de validation du formulaire.
+const layoutForm = z.object( {
+	font: z.enum( [ "inter", "manrope", "system" ] ),
+	theme: z.enum( [ "light", "dark" ] )
 } );
 
-type AppearanceFormValues = z.infer<typeof appearanceFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<AppearanceFormValues> = {
-	theme: "light"
-};
-
-export function AppearanceForm()
+export default function Layout()
 {
-	const form = useForm<AppearanceFormValues>( {
-		resolver: zodResolver( appearanceFormSchema ),
-		defaultValues
+	// Déclaration des variables d'état.
+	const [ isLoading, setIsLoading ] = useState( false );
+
+	// Mise à jour des informations.
+	const updateLayout = ( data: z.infer<typeof layoutForm> ) =>
+	{
+		setIsLoading( true );
+
+		setTimeout( () =>
+		{
+			toast( {
+				title: "Vous avez soumis les informations suivantes :",
+				description: (
+					<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+						<code className="text-white">
+							{JSON.stringify( data, null, 4 )}
+						</code>
+					</pre>
+				)
+			} );
+
+			setIsLoading( false );
+		}, 3000 );
+	};
+
+	// Définition du formulaire.
+	const form = useForm<z.infer<typeof layoutForm>>( {
+		resolver: zodResolver( layoutForm ),
+		defaultValues: {
+			font: "inter",
+			theme: "light"
+		}
 	} );
 
-	function onSubmit( data: AppearanceFormValues )
-	{
-		toast( {
-			title: "You submitted the following values:",
-			description: (
-				<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-					<code className="text-white">
-						{JSON.stringify( data, null, 2 )}
-					</code>
-				</pre>
-			)
-		} );
-	}
-
+	// Affichage du rendu HTML du composant.
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit( onSubmit )} className="space-y-8">
+			<form
+				onSubmit={form.handleSubmit( updateLayout )}
+				className="space-y-8"
+			>
+				{/* Police de caractère */}
 				<FormField
-					control={form.control}
 					name="font"
+					control={form.control}
 					render={( { field } ) => (
 						<FormItem>
-							<FormLabel>Font</FormLabel>
+							<FormLabel>Police de caractère</FormLabel>
+
 							<div className="relative w-max">
 								<FormControl>
 									<select
-										className={cn(
+										className={merge(
 											buttonVariants( {
 												variant: "outline"
 											} ),
@@ -76,29 +91,39 @@ export function AppearanceForm()
 										{...field}
 									>
 										<option value="inter">Inter</option>
-										<option value="manrope">Manrope</option>
+										<option value="poppins">Poppins</option>
 										<option value="system">System</option>
 									</select>
 								</FormControl>
-								<ChevronDownIcon className="absolute right-3 top-2.5 h-4 w-4 opacity-50" />
+
+								<ChevronDown className="absolute right-3 top-2.5 h-4 w-4 opacity-50" />
 							</div>
+
 							<FormDescription>
-								Set the font you want to use in the dashboard.
+								Définissez la police que vous souhaitez utiliser
+								sur l&lsquo;ensemble des pages du site.
 							</FormDescription>
+
 							<FormMessage />
 						</FormItem>
 					)}
 				/>
+
+				{/* Thème */}
 				<FormField
-					control={form.control}
 					name="theme"
+					control={form.control}
 					render={( { field } ) => (
 						<FormItem className="space-y-1">
-							<FormLabel>Theme</FormLabel>
+							<FormLabel>Thème</FormLabel>
+
 							<FormDescription>
-								Select the theme for the dashboard.
+								Définissez le thème que vous souhaitez utiliser
+								sur l&lsquo;ensemble des pages du site.
 							</FormDescription>
+
 							<FormMessage />
+
 							<RadioGroup
 								onValueChange={field.onChange}
 								defaultValue={field.value}
@@ -112,27 +137,32 @@ export function AppearanceForm()
 												className="sr-only"
 											/>
 										</FormControl>
+
 										<div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
 											<div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
 												<div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
 													<div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
 													<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
 												</div>
+
 												<div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
 													<div className="h-4 w-4 rounded-full bg-[#ecedef]" />
 													<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
 												</div>
+
 												<div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
 													<div className="h-4 w-4 rounded-full bg-[#ecedef]" />
 													<div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
 												</div>
 											</div>
 										</div>
+
 										<span className="block w-full p-2 text-center font-normal">
-											Light
+											Clair
 										</span>
 									</FormLabel>
 								</FormItem>
+
 								<FormItem>
 									<FormLabel className="[&:has([data-state=checked])>div]:border-primary">
 										<FormControl>
@@ -141,24 +171,28 @@ export function AppearanceForm()
 												className="sr-only"
 											/>
 										</FormControl>
+
 										<div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
 											<div className="space-y-2 rounded-sm bg-slate-950 p-2">
 												<div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
 													<div className="h-2 w-[80px] rounded-lg bg-slate-400" />
 													<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
 												</div>
+
 												<div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
 													<div className="h-4 w-4 rounded-full bg-slate-400" />
 													<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
 												</div>
+
 												<div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
 													<div className="h-4 w-4 rounded-full bg-slate-400" />
 													<div className="h-2 w-[100px] rounded-lg bg-slate-400" />
 												</div>
 											</div>
 										</div>
+
 										<span className="block w-full p-2 text-center font-normal">
-											Dark
+											Sombre
 										</span>
 									</FormLabel>
 								</FormItem>
@@ -167,7 +201,15 @@ export function AppearanceForm()
 					)}
 				/>
 
-				<Button type="submit">Update preferences</Button>
+				{/* Bouton de validation du formulaire */}
+				<Button disabled={isLoading}>
+					{isLoading ? (
+						<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+					) : (
+						<RefreshCw className="mr-2 h-4 w-4" />
+					)}
+					Mettre à jour
+				</Button>
 			</form>
 		</Form>
 	);
--- app/settings/layout/page.tsx
+++ app/settings/layout/page.tsx
@@ -1,21 +1,37 @@
-import { Separator } from "@/registry/new-york/ui/separator";
-import { AppearanceForm } from "@/app/examples/forms/appearance/appearance-form";
+//
+// Route vers la page de l'apparence du site.
+//
 
+// Importation des dépendances.
+import { lazy } from "react";
+
+// Importation des composants.
+import { Separator } from "../../components/ui/separator";
+
+const Layout = lazy( () => import( "../components/layout" ) );
+
+// Affichage de la page.
 export default function Page()
 {
+	// Affichage du rendu HTML de la page.
 	return (
 		<div className="space-y-6">
-			<div>
-				<h3 className="text-lg font-medium">Appearance</h3>
-				<p className="text-sm text-muted-foreground">
-					Customize the appearance of the app. Automatically switch
-					between day and night themes.
-				</p>
-			</div>
+			{/* En-tête de la page */}
+			<header>
+				<h3 className="text-lg font-medium">Apparence</h3>
 
+				<p className="text-sm text-muted-foreground">
+					Personnaliser l&lsquo;apparence du site. Basculer
+					automatiquement du thème clair au thème sombre et changer la
+					police de caractère.
+				</p>
+			</header>
+
+			{/* Barre verticale de séparation */}
 			<Separator />
 
-			<AppearanceForm />
+			{/* Formulaire de modification de l'apparence */}
+			<Layout />
 		</div>
 	);
 }
