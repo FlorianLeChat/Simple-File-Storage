54c629f6b907dc1cdffe57cb6276646ef47d82c1
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-07 18:23:40 +0200

    Improved file status indication on the dashboard page

--- app/dashboard/components/columns.tsx
+++ app/dashboard/components/columns.tsx
@@ -14,6 +14,7 @@ import { Trash,
 	ClipboardCopy,
 	MoreHorizontal } from "lucide-react";
 
+import { Badge } from "../../components/ui/badge";
 import { Checkbox } from "../../components/ui/checkbox";
 import ColumnHeader from "./column-header";
 import ShareManager from "./share-manager";
@@ -47,7 +48,7 @@ export type File = {
 	type: string;
 	size: number;
 	date: string;
-	status: "public" | "private";
+	status: "public" | "private" | "shared";
 };
 
 // Déclaration des colonnes du tableau.
@@ -117,7 +118,33 @@ export const columns: ColumnDef<File>[] = [
 	{
 		// Statut de partage du fichier.
 		accessorKey: "status",
-		header: ( { column } ) => <ColumnHeader column={column} title="Statut" />
+		header: ( { column } ) => <ColumnHeader column={column} title="Statut" />,
+		cell: ( { row } ) =>
+		{
+			const status: string = row.getValue( "status" );
+			const getColor = () =>
+			{
+				switch ( status )
+				{
+					// Fichier privé.
+					case "private":
+						return "bg-red-700 hover:bg-red-900";
+
+					// Fichier public.
+					case "public":
+						return "bg-green-700 hover:bg-green-900";
+
+					// Fichier partagé.
+					case "shared":
+						return "bg-yellow-700 hover:bg-yellow-900";
+
+					default:
+						return "";
+				}
+			};
+
+			return <Badge className={getColor()}>{status}</Badge>;
+		}
 	},
 	{
 		// Actions disponibles sur le fichier.
