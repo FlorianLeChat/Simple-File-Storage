653a2da2140becdb24a1561ffa77aa7bf98f69aa
Florian Trayon <git@florian-trayon.fr>
2023-10-08 20:49:46 +0200

    Improved layout of share manager elements on mobile devices

---
+++ app/components/ui/hover-card.tsx
@@ -0,0 +1,31 @@
+//
+// Composant générique des cartes de contenu.
+//  Source : https://ui.shadcn.com/docs/components/hover-card
+//
+import { merge } from "@/utilities/tailwind";
+import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
+import { forwardRef,
+	type ElementRef,
+	type ComponentPropsWithoutRef } from "react";
+
+const HoverCard = HoverCardPrimitive.Root;
+const HoverCardTrigger = HoverCardPrimitive.Trigger;
+
+const HoverCardContent = forwardRef<
+	ElementRef<typeof HoverCardPrimitive.Content>,
+	ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+>( ( { className, align = "center", sideOffset = 4, ...props }, ref ) => (
+	<HoverCardPrimitive.Content
+		ref={ref}
+		align={align}
+		sideOffset={sideOffset}
+		className={merge(
+			"z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+			className
+		)}
+		{...props}
+	/>
+) );
+HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
+
+export { HoverCard, HoverCardTrigger, HoverCardContent };
