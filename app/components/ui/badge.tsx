041fe9781d62564dd8a5a18785598feb9790e2e5
Florian Trayon <git@florian-trayon.fr>
2023-10-13 13:32:58 +0200

    Fixed colors of some texts and buttons with light theme

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -12,7 +12,7 @@ const badgeVariants = cva(
 		variants: {
 			variant: {
 				default:
-					"border-transparent bg-primary text-secondary-foreground hover:bg-primary/80",
+					"border-transparent bg-primary text-primary-foreground dark:text-secondary-foreground hover:bg-primary/80",
 				secondary:
 					"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				destructive:
