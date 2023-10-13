60d5ff6ecfa6574565012c1016e4ef44825a6107
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-13 13:31:23 +0200

    Added a button on page headers to view new notifications

--- app/components/user-menu.tsx
+++ app/components/user-menu.tsx
@@ -7,9 +7,16 @@
 import Link from "next/link";
 import { merge } from "@/utilities/tailwind";
 import { useRouter } from "next/navigation";
+import { BellRing, Check } from "lucide-react";
 import { useEffect, useCallback, useState } from "react";
 
-import { buttonVariants } from "./ui/button";
+import { Dialog,
+	DialogTitle,
+	DialogHeader,
+	DialogFooter,
+	DialogTrigger,
+	DialogContent,
+	DialogDescription } from "./ui/dialog";
 import { DropdownMenu,
 	DropdownMenuItem,
 	DropdownMenuGroup,
@@ -18,7 +25,29 @@ import { DropdownMenu,
 	DropdownMenuTrigger,
 	DropdownMenuShortcut,
 	DropdownMenuSeparator } from "./ui/dropdown-menu";
+import { Button, buttonVariants } from "./ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+
+const notifications = [
+	{
+		id: 1,
+		type: "success",
+		title: "Nouveau fichier téléversé",
+		description: "Votre fichier a bien été téléversé."
+	},
+	{
+		id: 2,
+		type: "warning",
+		title: "Collaborateur supprimé",
+		description: "Donald a été supprimé de votre espace de travail."
+	},
+	{
+		id: 3,
+		type: "error",
+		title: "Espace de stockage saturé",
+		description: "Vous avez atteint 90% de votre espace de stockage."
+	}
+];
 
 export default function UserMenu()
 {
@@ -87,7 +116,60 @@ export default function UserMenu()
 
 	// Affichage du rendu HTML du composant.
 	return (
-		<nav className="flex items-center space-x-4 sm:ml-auto">
+		<nav className="flex items-center justify-center space-x-4 md:ml-auto">
+			<Dialog>
+				<DialogTrigger
+					className={buttonVariants( { variant: "secondary" } )}
+				>
+					<BellRing className="inline h-5 w-5 md:mr-2" />
+
+					<span className="hidden md:inline">
+						Nouvelles notifications
+					</span>
+				</DialogTrigger>
+
+				<DialogContent className="w-[380px]">
+					<DialogHeader>
+						<DialogTitle>
+							<BellRing className="mr-2 inline h-5 w-5" />
+							Notifications
+						</DialogTitle>
+
+						<DialogDescription>
+							Vous avez 3 nouvelles notifications.
+						</DialogDescription>
+					</DialogHeader>
+
+					<ul className="my-2 flex flex-col gap-4">
+						{notifications.map( ( notification ) => (
+							<li
+								key={notification.id}
+								className="grid grid-cols-[25px_1fr]"
+							>
+								<span className="h-2 w-2 translate-y-1 rounded-full bg-primary" />
+
+								<div className="space-y-1">
+									<p className="text-sm font-medium leading-none">
+										{notification.title}
+									</p>
+
+									<p className="text-sm text-muted-foreground">
+										{notification.description}
+									</p>
+								</div>
+							</li>
+						) )}
+					</ul>
+
+					<DialogFooter>
+						<Button className="w-full">
+							<Check className="mr-2 h-4 w-4" />
+							Tout marquer comme lu
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+
 			<DropdownMenu open={open} onOpenChange={setOpen}>
 				{/* Bouton d'apparition */}
 				<DropdownMenuTrigger
