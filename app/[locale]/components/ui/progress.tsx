4128a0e8b82295897d8b2543120aee8ddd790df0
Florian Trayon <git@florian-trayon.fr>
2023-11-06 16:54:06 +0100

    Added client component for uploading files from the dashboard

---
+++ app/[locale]/components/ui/progress.tsx
@@ -0,0 +1,31 @@
+//
+// Composant générique de la barre de progression.
+//  Source : https://ui.shadcn.com/docs/components/progress
+//
+import { merge } from "@/utilities/tailwind";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { forwardRef,
+	type ElementRef,
+	type ComponentPropsWithoutRef } from "react";
+
+const Progress = forwardRef<
+	ElementRef<typeof ProgressPrimitive.Root>,
+	ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>( ( { className, value, ...props }, ref ) => (
+	<ProgressPrimitive.Root
+		ref={ref}
+		className={merge(
+			"relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+			className
+		)}
+		{...props}
+	>
+		<ProgressPrimitive.Indicator
+			className="h-full w-full flex-1 bg-primary transition-all"
+			style={{ transform: `translateX(-${ 100 - ( value || 0 ) }%)` }}
+		/>
+	</ProgressPrimitive.Root>
+) );
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
