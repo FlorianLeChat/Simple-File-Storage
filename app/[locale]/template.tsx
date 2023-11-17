c0bdccf5c01995a57cff31489ec718fb09fbd9c5
Florian Trayon <git@florian-trayon.fr>
2023-11-17 09:49:16 +0100

    Moved Next Auth error codes to a proper file for future translations

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -7,7 +7,12 @@
 
 // Importation des dépendances.
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
+
+// Importation des fonctions utilitaires.
+import { getAuthErrorMessage,
+	getGenericErrorMessage } from "@/utilities/next-auth";
 
 // Importation des composants.
 import { useToast } from "./components/ui/use-toast";
@@ -15,6 +20,7 @@ import { useToast } from "./components/ui/use-toast";
 export default function Template( { children }: { children: ReactNode } )
 {
 	// Déclaration des constantes.
+	const pathname = usePathname();
 	const { toast } = useToast();
 
 	// Affichage automatique des messages d'erreur.
@@ -22,35 +28,15 @@ export default function Template( { children }: { children: ReactNode } )
 	{
 		// On récupère d'abord le message d'erreur dans l'URL.
 		const parameters = new URLSearchParams( window.location.search );
-		const error = parameters.get( "error" );
+		const error = parameters.get( "error" ) ?? "";
 
-		// On définit ensuite le message d'erreur à afficher en fonction
-		//  des informations récupérées.
-		//  Source : https://authjs.dev/guides/basics/pages#error-page
-		let description: string | undefined;
+		// On affiche ensuite le message d'erreur en fonction de la page
+		//  sur laquelle où se trouve l'utilisateur.
+		const description =
+			pathname === "/"
+				? getGenericErrorMessage( error )
+				: pathname === "/auth" && getAuthErrorMessage( error );
 
-		switch ( error )
-		{
-			case "Configuration":
-				description =
-					"Un problème est survenu lors de la lecture de la configuration du site. Veuillez vérifier les journaux d'événements pour plus d'informations.";
-				break;
-
-			case "AccessDenied":
-				description =
-					"Vous n'avez pas les autorisations nécessaires pour vous authentifier sur ce site.";
-				break;
-
-			case "Verification":
-				description =
-					"Votre jeton de vérification est invalide ou a expiré. Veuillez vous reconnecter.";
-				break;
-
-			default:
-				break;
-		}
-
-		// On affiche enfin le message d'erreur s'il est défini.
 		if ( description )
 		{
 			setTimeout( () =>
@@ -62,7 +48,7 @@ export default function Template( { children }: { children: ReactNode } )
 				} );
 			}, 0 );
 		}
-	}, [ toast ] );
+	}, [ toast, pathname ] );
 
 	// Affichage du rendu HTML de la page.
 	return children;
