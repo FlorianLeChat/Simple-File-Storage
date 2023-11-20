b09ac65bcc48e14d8894a98c7c36760c3a073f4e
Florian Trayon <git@florian-trayon.fr>
2023-11-20 09:44:29 +0100

    Added dynamic metadata generation of all pages with GitHub API

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -3,13 +3,17 @@
 //  Source : https://nextjs.org/docs/app/api-reference/file-conventions/loading
 //
 import { Loader2 } from "lucide-react";
+import { generateMetadata } from "./layout";
 
-export default function Loading()
+export default async function Loading()
 {
+	// Déclaration des constantes.
+	const { title } = ( await generateMetadata() ) as { title: string };
+
 	// Affichage du rendu HTML du composant.
 	return (
 		<div className="m-2 flex h-[calc(100vh-1rem)] flex-col items-center justify-center gap-4 text-center text-4xl font-bold uppercase leading-normal">
-			💾 Simple File Storage
+			💾 {title}
 			<Loader2 className="h-14 w-14 animate-spin text-primary" />
 		</div>
 	);
