f4441c95aecd93bd01b1b1830b2494cfd2f804ab
Florian Trayon <git@florian-trayon.fr>
2023-11-21 12:33:09 +0100

    Improved Zod validation scheme for authentication form

--- schemas/authentication.ts
+++ schemas/authentication.ts
@@ -3,20 +3,15 @@
 //
 import { z } from "zod";
 
-// Validation requise pour le mot de passe (authentification par courriel).
-const requiredPassword = z.string().min( 10 ).max( 60 );
-// Validation optionnelle pour le mot de passe (authentification par mot de passe).
-const optionalPassword = z.string().optional().optional();
-
 const schema = z.object( {
 	// Adresse électronique.
 	email: z.string().min( 10 ).max( 100 ).email(),
 
-	// Mot de passe (requis ou optionnel, optionnel par défaut).
-	password: optionalPassword,
+	// Mot de passe.
+	password: z.string().min( 10 ).max( 60 ).or( z.literal( "" ) ),
 
 	// Se souvenir de moi.
 	remembered: z.boolean().optional()
 } );
 
-export { schema, requiredPassword, optionalPassword };
+export default schema;
