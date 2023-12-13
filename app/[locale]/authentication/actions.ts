da99f047d686dd0cef5f5d4e374fe7c72909421b
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-12-13 18:51:36 +0100

    Improved authentication mechanism using NextJS server actions

--- app/[locale]/authentication/actions.ts
+++ app/[locale]/authentication/actions.ts
@@ -7,6 +7,8 @@
 
 import prisma from "@/utilities/prisma";
 import schema from "@/schemas/authentication";
+import { signIn } from "@/utilities/next-auth";
+import { redirect } from "next/navigation";
 
 //
 // Enregistrement d'un nouveau compte utilisateur.
@@ -16,6 +18,8 @@ export async function signUpAccount(
 	formData: FormData
 )
 {
+	// On tente d'abord de valider les informations d'authentification
+	//  fournies par l'utilisateur.
 	const result = schema.safeParse( {
 		email: formData.get( "email" ),
 		password: formData.get( "password" )
@@ -23,12 +27,16 @@ export async function signUpAccount(
 
 	if ( !result.success )
 	{
+		// Si les informations d'authentification fournies sont invalides,
+		//  on affiche un message d'erreur sur la page d'authentification.
 		return {
 			success: false,
 			reason: result.error.issues[ 0 ].message
 		};
 	}
 
+	// Dans le cas contraire, on vérifie si un utilisateur existe déjà
+	//  dans la base de données avec l'adresse électronique fournie.
 	const user = await prisma.user.findUnique( {
 		where: {
 			email: result.data.email
@@ -37,12 +45,16 @@ export async function signUpAccount(
 
 	if ( user )
 	{
+		// Si c'est le cas, on indique à l'utilisateur que l'adresse
+		//  électronique fournie est déjà utilisée.
 		return {
 			success: false,
 			reason: "OAuthAccountNotLinked"
 		};
 	}
 
+	// On tente ensuite de créer un nouveau compte utilisateur via
+	//  les informations d'authentification fournies.
 	const validation = await prisma.verificationToken.findFirst( {
 		where: {
 			identifier: result.data.email,
@@ -57,6 +69,8 @@ export async function signUpAccount(
 		// TODO : Envoyer un courriel de validation.
 	}
 
+	// On retourne enfin un message de succès à l'utilisateur afin
+	//  qu'il puisse valider son adresse électronique.
 	return {
 		success: true,
 		reason: "ValidationRequired"
@@ -71,8 +85,80 @@ export async function signInAccount(
 	formData: FormData
 )
 {
+	// On vérifie d'abord si l'utilisateur a tenté de se connecter via
+	//  un fournisseur d'authentification externe.
+	const provider = formData.get( "provider" );
+
+	if ( provider )
+	{
+		try
+		{
+			// Si c'est le cas, on tente une authentification via le fournisseur
+			//  d'authentification externe avant de rediriger l'utilisateur vers
+			//  la page de son tableau de bord.
+			redirect(
+				await signIn( provider as string, {
+					redirect: false,
+					redirectTo: "/dashboard"
+				} )
+			);
+		}
+		catch ( error )
+		{
+			// En cas d'erreur, on affiche un message d'erreur sur la page
+			//  d'authentification.
+			return {
+				success: false,
+				reason: "OAuthCallbackError"
+			};
+		}
+	}
+
+	// Dans le cas contraire, on tente de valider les informations
+	//  d'authentification fournies par l'utilisateur.
+	const result = schema.safeParse( {
+		email: formData.get( "email" ),
+		password: formData.get( "password" )
+	} );
+
+	if ( !result.success )
+	{
+		// Si les informations d'authentification fournies sont invalides,
+		//  on affiche un message d'erreur sur la page d'authentification.
+		return {
+			success: false,
+			reason: result.error.issues[ 0 ].message
+		};
+	}
+
+	try
+	{
+		// Dans le cas contraire, on tente alors une authentification via
+		//  les informations d'authentification fournies avant de rediriger
+		//  l'utilisateur vers la page de son tableau de bord.
+		redirect(
+			await signIn( "credentials", {
+				email: result.data.email,
+				password: result.data.password,
+				redirect: false,
+				redirectTo: "/dashboard"
+			} )
+		);
+	}
+	catch ( error )
+	{
+		// En cas d'erreur, on affiche un message d'erreur sur la page
+		//  d'authentification.
+		return {
+			success: false,
+			reason: "CredentialsSignin"
+		};
+	}
+
+	// On retourne enfin un message d'erreur par défaut au l'utilisateur
+	//  ne correspondant à aucun des cas précédents.
 	return {
-		success: true,
-		reason: "ValidationRequired"
+		success: false,
+		reason: "CredentialsSignin"
 	};
 }
