d96deb59aa943e18f2a69ab7b383264f9a24723d
Florian Trayon <git@florian-trayon.fr>
2024-01-03 11:22:07 +0100

    Moved some utility functions to a dedicated folder

---
+++ utilities/react-table.ts
@@ -0,0 +1,19 @@
+//
+// Conversion d'une taille de fichier en une chaîne de caractères.
+//  Source : https://stackoverflow.com/q/10420352
+//
+const units = [ "B", "KB", "MB", "GB", "TB" ];
+
+export function formatSize( raw: number ): string
+{
+	let index = 0;
+	let size = raw;
+
+	while ( size >= 1024 && index < units.length - 1 )
+	{
+		size /= 1024;
+		index++;
+	}
+
+	return `${ parseFloat( size.toFixed( 2 ) ).toLocaleString() } ${ units[ index ] }`;
+}
