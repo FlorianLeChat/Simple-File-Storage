da0d1f3f93c5a807a2961c9024a2490b0535d835
Florian Trayon <git@florian-trayon.fr>
2024-01-04 10:24:09 +0100

    Improved action server responses for unauthenticated users

--- app/[locale]/settings/notifications/actions.ts
+++ app/[locale]/settings/notifications/actions.ts
@@ -7,7 +7,6 @@
 import prisma from "@/utilities/prisma";
 import schema from "@/schemas/notifications";
 import { auth } from "@/utilities/next-auth";
-import { redirect } from "next/navigation";
 
 //
 // Mise à jour des notifications.
@@ -22,9 +21,12 @@ export async function updateNotifications(
 
 	if ( !session )
 	{
-		// Si la session n'existe pas, on redirige l'utilisateur vers
-		//  la page d'accueil.
-		return redirect( "/" );
+		// Si la session n'existe pas, on indique que l'utilisateur
+		//  n'est pas connecté.
+		return {
+			success: false,
+			reason: "form.errors.unauthenticated"
+		};
 	}
 
 	// On tente ensuite de valider les données du formulaire.
