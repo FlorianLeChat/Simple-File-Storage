75935253cb7b7fc01f9d38bd95de3cb6cd43c6e2
Florian Trayon <git@florian-trayon.fr>
2024-01-15 20:15:03 +0100

    Moved user quota state to data table context for automated updates

--- @types/react-table.d.ts
+++ @types/react-table.d.ts
@@ -8,11 +8,17 @@ declare module "@tanstack/table-core" {
 		// Fichiers affichés dans le tableau.
 		files: FileAttributes[];
 
+		// Taille du quota utilisateur.
+		quota: number;
+
 		// Lignes en cours de chargement.
 		loading: string[];
 
 		// Fonction de mise à jour des fichiers.
 		setFiles: Dispatch<SetStateAction<FileAttributes[]>>;
+
+		// Fonction de mise à jour du quota.
+		setQuota: Dispatch<SetStateAction<number>>;
 
 		// Fonction de mise à jour de l'état de chargement.
 		setLoading: Dispatch<SetStateAction<string[]>>;
