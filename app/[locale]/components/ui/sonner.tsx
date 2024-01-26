eb40a4e76f52163b3a3c1391ca37b76e230201dd
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-01-26 10:54:25 +0100

    Improved notification layout when responding to server actions

---
+++ app/[locale]/components/ui/sonner.tsx
@@ -0,0 +1,36 @@
+//
+// Composant générique des notifications.
+//  Source : https://ui.shadcn.com/docs/components/sonner
+//
+
+"use client";
+
+import { Toaster as Sonner } from "sonner";
+import type { ComponentProps } from "react";
+
+type ToasterProps = ComponentProps<typeof Sonner>;
+
+function Toaster( { ...props }: ToasterProps )
+{
+	return (
+		<Sonner
+			position="bottom-center"
+			className="toaster group"
+			richColors
+			closeButton
+			toastOptions={{
+				classNames: {
+					toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+					description: "group-[.toast]:text-muted-foreground",
+					actionButton:
+						"group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+					cancelButton:
+						"group-[.toast]:bg-muted group-[.toast]:text-muted-foreground"
+				}
+			}}
+			{...props}
+		/>
+	);
+}
+
+export default Toaster;
