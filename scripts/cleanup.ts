ef37bca8809efdbf24a4a04d83b047e3261a74fa
Florian Trayon <git@florian-trayon.fr>
2024-01-27 10:01:13 +0100

    Added CLI script to delete expired user files with automated jobs

---
+++ scripts/cleanup.ts
@@ -0,0 +1,69 @@
+//
+// Suppression des fichiers expirés dans le système de fichiers et dans la base de données.
+//
+
+// Importation des dépendances.
+import { join } from "path";
+import * as dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { existsSync } from "fs";
+import { readdir, rm } from "fs/promises";
+
+// Configuration des variables d'environnement.
+dotenv.config( { path: join( process.cwd(), ".env.local" ), override: true } );
+
+// Exécution de la fonction asynchrone.
+const cleanup = async () =>
+{
+	// Création d'une nouvelle instance de Prisma et récupération
+	//  des fichiers expirés depuis la base de données.
+	const prisma = new PrismaClient();
+	const files = await prisma.file.findMany( {
+		where: {
+			expiration: {
+				lte: new Date()
+			}
+		}
+	} );
+
+	// Suppression des fichiers expirés dans la base de données.
+	await prisma.file.deleteMany( {
+		where: {
+			id: {
+				in: files.map( ( file ) => file.id )
+			}
+		}
+	} );
+
+	// Création d'une promesse pour traiter chaque fichier.
+	await Promise.all(
+		files.map( async ( file ) =>
+		{
+			// Récupération du répertoire utilisateur.
+			const userFolder = join( process.cwd(), "public/files", file.userId );
+
+			if ( existsSync( userFolder ) )
+			{
+				// Récupération du chemin d'accès au fichier.
+				const fileFolder = join( userFolder, file.id );
+
+				if ( existsSync( fileFolder ) )
+				{
+					// Suppression du fichier dans le système de fichiers.
+					await rm( fileFolder, { recursive: true, force: true } );
+				}
+
+				if ( ( await readdir( userFolder ) ).length === 0 )
+				{
+					// Suppression du répertoire utilisateur si celui-ci est vide.
+					await rm( userFolder, { recursive: true, force: true } );
+				}
+			}
+		} )
+	);
+
+	// Déconnexion de la base de données.
+	prisma.$disconnect();
+};
+
+cleanup();
