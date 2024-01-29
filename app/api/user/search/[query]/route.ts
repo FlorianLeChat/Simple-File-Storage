d0e9c558f2556c89d359060998a730ad69afbe37
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-01-29 20:48:18 +0100

    Added an API route to search users by name or e-mail address

---
+++ app/api/user/search/[query]/route.ts
@@ -0,0 +1,51 @@
+//
+// Route pour la recherche d'utilisateurs via leur nom ou leur adresse électronique.
+//
+import prisma from "@/utilities/prisma";
+import { auth } from "@/utilities/next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(
+	_request: Request,
+	{ params }: { params: { query: string } }
+)
+{
+	// On vérifie si l'utilisateur est connecté afin de récupérer
+	//  ses informations.
+	const session = await auth();
+
+	if ( !session )
+	{
+		return new NextResponse( null, { status: 403 } );
+	}
+
+	// On récupère ensuite tous les utilisateurs dont le nom ou l'adresse
+	//  électronique contient la chaîne de caractères demandée.
+	const users = await prisma.user.findMany( {
+		where: {
+			OR: [
+				{
+					name: {
+						contains: params.query
+					}
+				},
+				{
+					email: {
+						contains: params.query
+					}
+				}
+			]
+		},
+		take: 10
+	} );
+
+	// On retourne enfin les données des utilisateurs comme une réponse JSON.
+	return NextResponse.json(
+		users.map( ( user ) => ( {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			image: user.image
+		} ) )
+	);
+}
