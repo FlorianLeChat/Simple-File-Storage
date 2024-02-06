fcdc13af2a8489ff55113fea8089cae51d4d37cd
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-02-06 12:58:28 +0100

    Added front-end file-sharing manager mechanism from dashboard

--- app/[locale]/dashboard/components/share-manager.tsx
+++ app/[locale]/dashboard/components/share-manager.tsx
@@ -6,6 +6,7 @@
 "use client";
 
 import useSWR from "swr";
+import { toast } from "sonner";
 import { useState } from "react";
 import { Trash,
 	Users,
@@ -13,6 +14,7 @@ import { Trash,
 	UserCog,
 	UserPlus,
 	ClipboardCopy } from "lucide-react";
+import serverAction from "@/utilities/recaptcha";
 import type { User } from "next-auth";
 import type { FileAttributes } from "@/interfaces/File";
 import type { Table, Row, TableMeta } from "@tanstack/react-table";
@@ -29,6 +31,7 @@ import { Avatar,
 	AvatarFallback } from "../../components/ui/avatar";
 import { Separator } from "../../components/ui/separator";
 import { ScrollArea } from "../../components/ui/scroll-area";
+import { addSharedUser, deleteSharedUser } from "../actions";
 
 export default function ShareManager( {
 	table,
@@ -84,12 +87,15 @@ export default function ShareManager( {
 				</h4>
 
 				{file.shares.length === 0 ? (
-					<p className="pt-4 text-sm text-muted-foreground">
+					<p className="mt-4 text-sm text-muted-foreground">
 						Aucun utilisateur n&lsquo;a accès à ce fichier.
 					</p>
 				) : (
 					file.shares.map( ( share ) => (
-						<article className="mt-3 flex flex-wrap items-center gap-3 max-sm:justify-center">
+						<article
+							key={share.user.uuid}
+							className="mt-4 flex flex-wrap items-center gap-3 max-sm:justify-center"
+						>
 							{/* Avatar de l'utilisateur */}
 							<Avatar>
 								<AvatarImage
@@ -115,38 +121,29 @@ export default function ShareManager( {
 
 							{/* Autorisations accordées */}
 							<div className="flex gap-3 sm:ml-auto">
-								<Select disabled={loading} defaultValue="write">
+								<Select
+									disabled={loading}
+									defaultValue={share.status}
+								>
 									<SelectTrigger className="ml-auto w-auto">
 										<SelectValue />
 									</SelectTrigger>
 
 									<SelectContent>
 										<SelectItem value="write">
-											{loading && (
-												<Loader2 className="mr-2 inline h-4 w-4 animate-spin" />
-											)}
-
-											<span className="mr-2">
+											<span className="mr-1">
 												Écriture
 											</span>
 										</SelectItem>
 
 										<SelectItem value="read">
-											{loading && (
-												<Loader2 className="mr-2 inline h-4 w-4 animate-spin" />
-											)}
-
-											<span className="mr-2">
+											<span className="mr-1">
 												Lecture
 											</span>
 										</SelectItem>
 
 										<SelectItem value="admin">
-											{loading && (
-												<Loader2 className="mr-2 inline h-4 w-4 animate-spin" />
-											)}
-
-											<span className="mr-2">
+											<span className="mr-1">
 												Gestion
 											</span>
 										</SelectItem>
@@ -154,10 +151,82 @@ export default function ShareManager( {
 								</Select>
 
 								<Button
+									onClick={async () =>
+									{
+										// Activation de l'état de chargement.
+										states.setLoading( [ "modal" ] );
+
+										// Création d'un formulaire de données.
+										const form = new FormData();
+										form.append( "fileId", file.uuid );
+										form.append(
+											"userId",
+											share.user.uuid ?? ""
+										);
+
+										// Envoi de la requête au serveur et
+										//  attente de la réponse.
+										const state = ( await serverAction(
+											deleteSharedUser,
+											form
+										) ) as boolean;
+
+										if ( state )
+										{
+											// Réinitialisation de la recherche.
+											setSearch( "" );
+
+											// Mise à jour de l'état du fichier.
+											if ( file.shares.length === 1 )
+											{
+												file.status = "private";
+												file.shares = [];
+											}
+											else
+											{
+												file.shares =
+													file.shares.filter(
+														( value ) => value.user.uuid
+															!== share.user.uuid
+													);
+											}
+
+											states.setFiles( [ ...states.files ] );
+										}
+
+										// Fin de l'état de chargement.
+										states.setLoading( [] );
+
+										// Envoi d'une notification.
+										if ( state )
+										{
+											toast.success(
+												"form.info.delete_success",
+												{
+													description:
+														"form.info.sharing_updated"
+												}
+											);
+										}
+										else
+										{
+											toast.error(
+												"form.errors.delete_failed",
+												{
+													description:
+														"form.errors.server_error"
+												}
+											);
+										}
+									}}
 									variant="destructive"
 									disabled={loading}
 								>
-									<Trash className="h-4 w-4" />
+									{loading ? (
+										<Loader2 className="h-4 w-4 animate-spin" />
+									) : (
+										<Trash className="h-4 w-4" />
+									)}
 								</Button>
 							</div>
 						</article>
@@ -191,7 +260,7 @@ export default function ShareManager( {
 					autoCapitalize="off"
 				/>
 
-				<p className="mt-4 text-sm text-muted-foreground">
+				<p className="mt-3 text-sm text-muted-foreground">
 					<strong>{data?.length ?? 0}</strong> résultat(s) trouvé(s)
 					dans la base de données.
 				</p>
@@ -246,10 +315,81 @@ export default function ShareManager( {
 
 									{/* Bouton d'ajout de l'utilisateur */}
 									<Button
+										onClick={async () =>
+										{
+											// Activation de l'état de chargement.
+											states.setLoading( [ "modal" ] );
+
+											// Création d'un formulaire de données.
+											const form = new FormData();
+											form.append( "fileId", file.uuid );
+											form.append(
+												"userId",
+												user.id ?? ""
+											);
+
+											// Envoi de la requête au serveur et
+											//  attente de la réponse.
+											const state = ( await serverAction(
+												addSharedUser,
+												form
+											) ) as boolean;
+
+											if ( state )
+											{
+												// Réinitialisation de la recherche.
+												setSearch( "" );
+
+												// Mise à jour de l'état du fichier.
+												file.status = "shared";
+												file.shares.push( {
+													user: {
+														uuid: user.id,
+														name: user.name,
+														email: user.email,
+														image: user.image
+													},
+													status: "read"
+												} );
+
+												states.setFiles( [
+													...states.files
+												] );
+											}
+
+											// Fin de l'état de chargement.
+											states.setLoading( [] );
+
+											// Envoi d'une notification.
+											if ( state )
+											{
+												toast.success(
+													"form.info.update_success",
+													{
+														description:
+															"form.info.sharing_updated"
+													}
+												);
+											}
+											else
+											{
+												toast.error(
+													"form.errors.update_failed",
+													{
+														description:
+															"form.errors.server_error"
+													}
+												);
+											}
+										}}
 										disabled={loading}
-										className="mr-4 sm:ml-auto"
+										className="sm:ml-auto"
 									>
-										<UserPlus className="mr-2 h-4 w-4" />
+										{loading ? (
+											<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+										) : (
+											<UserPlus className="mr-2 h-4 w-4" />
+										)}
 										Ajouter
 									</Button>
 
