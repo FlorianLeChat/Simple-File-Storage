51c3bf081b17f5846beef5dc438273509facce53
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-02-08 22:16:05 +0100

    Added additional query string parameters for dashboard data

--- app/[locale]/dashboard/components/column-header.tsx
+++ app/[locale]/dashboard/components/column-header.tsx
@@ -4,6 +4,7 @@
 //
 import { merge } from "@/utilities/tailwind";
 import { Column } from "@tanstack/react-table";
+import { useSearchParams } from "next/navigation";
 import { type HTMLAttributes } from "react";
 import { EyeOff, ArrowUp, ArrowDown, ArrowDownUp } from "lucide-react";
 
@@ -25,6 +26,13 @@ export default function ColumnHeader<TData, TValue>( {
 	title
 }: ColumnHeaderProps<TData, TValue> )
 {
+	// Déclaration des variables d'état.
+	const parameters = useSearchParams();
+	const ascending =
+		column.getIsSorted() === "asc" || parameters.get( "asc" ) === column.id;
+	const descending =
+		column.getIsSorted() === "desc" || parameters.get( "desc" ) === column.id;
+
 	// Vérification de la possibilité de trier la colonne.
 	if ( !column.getCanSort() )
 	{
@@ -42,12 +50,10 @@ export default function ColumnHeader<TData, TValue>( {
 						"-ml-3 h-8 data-[state=open]:bg-accent"
 					)}
 				>
-					<span>{title}</span>
+					{title}
 
-					{( column.getIsSorted() === "desc" && (
-						<ArrowDown className="ml-2 h-4 w-4" />
-					) )
-						|| ( column.getIsSorted() === "asc" ? (
+					{( descending && <ArrowDown className="ml-2 h-4 w-4" /> )
+						|| ( ascending ? (
 							<ArrowUp className="ml-2 h-4 w-4" />
 						) : (
 							<ArrowDownUp className="ml-2 h-4 w-4" />
@@ -57,14 +63,36 @@ export default function ColumnHeader<TData, TValue>( {
 				{/* Options de tri disponibles */}
 				<DropdownMenuContent align="start">
 					<DropdownMenuItem
-						onClick={() => column.toggleSorting( false )}
+						onClick={() =>
+						{
+							// Inversion du tri de la colonne.
+							column.toggleSorting( false );
+
+							// Mise à jour de l'URL.
+							const url = new URLSearchParams( parameters );
+							url.set( "asc", column.id );
+							url.delete( "desc" );
+
+							window.history.pushState( null, "", `?${ url }` );
+						}}
 					>
 						<ArrowUp className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
 						Ascendant
 					</DropdownMenuItem>
 
 					<DropdownMenuItem
-						onClick={() => column.toggleSorting( true )}
+						onClick={() =>
+						{
+							// Inversion du tri de la colonne.
+							column.toggleSorting( true );
+
+							// Mise à jour de l'URL.
+							const url = new URLSearchParams( parameters );
+							url.set( "desc", column.id );
+							url.delete( "asc" );
+
+							window.history.pushState( null, "", `?${ url }` );
+						}}
 					>
 						<ArrowDown className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
 						Descendant
@@ -73,7 +101,17 @@ export default function ColumnHeader<TData, TValue>( {
 					<DropdownMenuSeparator />
 
 					<DropdownMenuItem
-						onClick={() => column.toggleVisibility( false )}
+						onClick={() =>
+						{
+							// Masquage de la colonne.
+							column.toggleVisibility( false );
+
+							// Mise à jour de l'URL.
+							const url = new URLSearchParams( parameters );
+							url.append( "hide", column.id );
+
+							window.history.pushState( null, "", `?${ url }` );
+						}}
 					>
 						<EyeOff className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
 						Cacher
