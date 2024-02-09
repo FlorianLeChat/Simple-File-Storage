f7435365436927650e55bb8ae1e0c25af8b963ec
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-02-09 20:46:43 +0100

    Improved description of user file attribute TypeScript interfaces

---
+++ interfaces/Share.ts
@@ -0,0 +1,24 @@
+//
+// Interface des attributs des partages de fichiers.
+//
+import type { User } from "next-auth";
+
+export interface ShareAttributes {
+	// Utilisateur en partage.
+	user: {
+		// Identifiant unique.
+		uuid: User["id"];
+
+		// Nom d'affichage.
+		name: User["name"];
+
+		// Adresse électronique.
+		email: User["email"];
+
+		// Image de profil.
+		image: User["image"];
+	};
+
+	// Type de partage du fichier avec l'utilisateur.
+	status: "read" | "write" | "admin";
+}
---
+++ interfaces/Version.ts
@@ -0,0 +1,16 @@
+//
+// Interface des attributs des versions de fichiers.
+//
+export interface VersionAttributes {
+	// Identifiant unique de la version.
+	uuid: string;
+
+	// Poids de la version en octets.
+	size: number;
+
+	// Date de création de la version.
+	date: Date;
+
+	// Chemin d'accès à la version.
+	path: string;
+}
