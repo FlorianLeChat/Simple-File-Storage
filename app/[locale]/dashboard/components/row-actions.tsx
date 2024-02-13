edb9d27da502eda1568741d4fb9356ef1dfbaa2c
Florian Trayon <git@florian-trayon.fr>
2024-02-13 09:35:09 +0100

    Moved screen reader text to HTML attributes for better accessibility

--- app/[locale]/dashboard/components/row-actions.tsx
+++ app/[locale]/dashboard/components/row-actions.tsx
@@ -101,21 +101,16 @@ export default function RowActions( {
 		>
 			{/* Bouton d'ouverture du menu */}
 			<DropdownMenuTrigger
+				title={loading ? "Mise à jour en cours..." : "Ouvrir le menu"}
 				className={merge(
 					buttonVariants( { variant: "ghost" } ),
 					"h-8 w-8 p-0"
 				)}
 			>
 				{loading ? (
-					<>
-						<span className="sr-only">Mise à jour en cours...</span>
-						<Loader2 className="h-4 w-4 animate-spin" />
-					</>
+					<Loader2 className="h-4 w-4 animate-spin" />
 				) : (
-					<>
-						<span className="sr-only">Ouvrir le menu</span>
-						<MoreHorizontal className="h-4 w-4" />
-					</>
+					<MoreHorizontal className="h-4 w-4" />
 				)}
 			</DropdownMenuTrigger>
 
