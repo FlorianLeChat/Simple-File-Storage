ceaf30a858e616455d251678a843cb8b6b1ece42
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-02-19 19:01:15 +0100

    Improved CLI script to delete outdated notifications periodically

--- scripts/outdated-notifications.ts
+++ scripts/outdated-notifications.ts
@@ -13,16 +13,38 @@ dotenv.config( { path: join( process.cwd(), ".env.local" ), override: true } );
 // Exécution de la fonction asynchrone.
 const script = async () =>
 {
-	// Suppression des notifications créées il y a plus de 30 jours.
+	// Récupération des utilisateurs.
 	const prisma = new PrismaClient();
-
-	await prisma.notification.findMany( {
-		where: {
-			createdAt: {
-				lte: new Date( Date.now() - 30 * 24 * 60 * 60 * 1000 )
+	const users = await prisma.user.findMany( {
+		include: {
+			_count: {
+				select: {
+					notifications: true
+				}
 			}
 		}
 	} );
+
+	await Promise.all(
+		users.map( async ( user ) =>
+		{
+			// Vérification du nombre de notifications.
+			if ( user._count.notifications > 50 )
+			{
+				// Suppression des notifications datant de plus de 30 jours.
+				await prisma.notification.deleteMany( {
+					where: {
+						userId: user.id,
+						createdAt: {
+							lte: new Date(
+								Date.now() - 30 * 24 * 60 * 60 * 1000
+							)
+						}
+					}
+				} );
+			}
+		} )
+	);
 
 	// Déconnexion de la base de données.
 	prisma.$disconnect();
