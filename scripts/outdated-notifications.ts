37c2e33e9959c3bf095defd625d55ed70a4e1c4e
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-02-19 20:24:54 +0100

    Reduced abuse of magic numbers in latest modifications

--- scripts/outdated-notifications.ts
+++ scripts/outdated-notifications.ts
@@ -10,6 +10,12 @@ import { PrismaClient } from "@prisma/client";
 // Configuration des variables d'environnement.
 dotenv.config( { path: join( process.cwd(), ".env.local" ), override: true } );
 
+// Nombre maximal de notifications que l'utilisateur peut avoir.
+const MAX_NOTIFICATIONS = 50;
+
+// Durée avant la suppression des notifications (en millisecondes).
+const TIME_BEFORE_DELETION = 30 * 24 * 60 * 60 * 1000;
+
 // Exécution de la fonction asynchrone.
 const script = async () =>
 {
@@ -29,16 +35,14 @@ const script = async () =>
 		users.map( async ( user ) =>
 		{
 			// Vérification du nombre de notifications.
-			if ( user._count.notifications > 50 )
+			if ( user._count.notifications > MAX_NOTIFICATIONS )
 			{
 				// Suppression des notifications datant de plus de 30 jours.
 				await prisma.notification.deleteMany( {
 					where: {
 						userId: user.id,
 						createdAt: {
-							lte: new Date(
-								Date.now() - 30 * 24 * 60 * 60 * 1000
-							)
+							lte: new Date( Date.now() - TIME_BEFORE_DELETION )
 						}
 					}
 				} );
