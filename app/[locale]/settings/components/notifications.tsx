dc17ff8589f064e809b05574d2eca52d0c306bea
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-03-02 09:17:15 +0100

    Fixed multiple accessibility issues with user notification settings

--- app/[locale]/settings/components/notifications.tsx
+++ app/[locale]/settings/components/notifications.tsx
@@ -42,10 +42,11 @@ export default function Notifications( { session }: { session: Session } )
 	} );
 
 	// Déclaration du formulaire.
+	const level = notifications[ 0 ] as "all" | "necessary" | "off";
 	const form = useForm( {
 		defaultValues: {
 			push: notifications[ 1 ] === "mail",
-			level: notifications[ 0 ] as "all" | "necessary" | "off"
+			level
 		}
 	} );
 
@@ -98,9 +99,26 @@ export default function Notifications( { session }: { session: Session } )
 	return (
 		<Form {...form}>
 			<form
-				action={( formData ) => serverAction( updateAction, formData )}
-				onSubmit={() => setLoading( true )}
-				className="space-y-6"
+				action={async ( formData ) =>
+				{
+					// Vérifications côté client.
+					const state = await form.trigger();
+
+					if ( !state )
+					{
+						return false;
+					}
+
+					// Activation de l'état de chargement.
+					setLoading( true );
+
+					// Récupération des données du formulaire.
+					formData.set( "level", form.getValues( "level" ) );
+
+					// Exécution de l'action côté serveur.
+					return serverAction( updateAction, formData );
+				}}
+				className="space-y-8"
 			>
 				{/* Activation des notifications par courriel */}
 				<FormField
@@ -108,14 +126,17 @@ export default function Notifications( { session }: { session: Session } )
 					control={form.control}
 					render={( { field } ) => (
 						<FormItem
-							className={`-mx-2 mb-2 flex items-center space-y-0 rounded-md border p-4 transition-opacity ${
+							className={`flex items-center gap-4 space-y-0 rounded-md border p-4 transition-opacity ${
 								!isPush ? "opacity-50" : ""
 							}`}
 						>
 							<Mail className="max-sm:hidden" />
 
-							<div className="flex-1 sm:ml-4">
-								<FormLabel className="text-sm font-medium leading-none">
+							<div className="flex-1">
+								<FormLabel
+									htmlFor={field.name}
+									className="text-sm font-medium leading-4"
+								>
 									Notifications par courriel
 								</FormLabel>
 
@@ -125,16 +146,10 @@ export default function Notifications( { session }: { session: Session } )
 								</FormDescription>
 							</div>
 
-							<input
-								// Support des actions de serveur pour NextJS.
-								// https://github.com/react-hook-form/react-hook-form/pull/11061
-								type="hidden"
-								name="push"
-								value={field.value ? "on" : "off"}
-							/>
-
 							<FormControl>
 								<Switch
+									id={field.name}
+									name={field.name}
 									checked={field.value}
 									disabled={isLoading || !isPush}
 									onCheckedChange={field.onChange}
@@ -151,116 +166,109 @@ export default function Notifications( { session }: { session: Session } )
 					name="level"
 					control={form.control}
 					render={( { field } ) => (
-						<FormItem>
-							<input
-								// Support des actions de serveur pour NextJS.
-								// https://github.com/react-hook-form/react-hook-form/pull/11061
-								type="hidden"
-								name="level"
-								value={field.value}
-							/>
+						<FormItem className="grid gap-6">
+							{/* Toutes les notifications */}
+							<div className="flex items-center gap-4">
+								<BellPlus className="min-w-fit max-sm:hidden" />
 
-							<FormControl>
-								<div className="grid gap-2">
-									<Button
-										{...field}
-										type="button"
-										variant={
-											field.value === "all"
-												? "secondary"
-												: "ghost"
-										}
-										onClick={() =>
-										{
-											form.setValue( "level", "all" );
-											setPush( true );
-										}}
-										disabled={isLoading}
-										className="-mx-2 h-auto justify-normal gap-4 p-3 text-left"
-									>
-										<BellPlus className="w-auto max-sm:hidden" />
+								<div>
+									<h4 className="mb-2 text-sm font-medium leading-4 sm:mb-1">
+										Toutes les notifications
+									</h4>
 
-										<div>
-											<h4 className="mb-2 text-sm font-medium leading-none sm:mb-1">
-												Toutes les notifications
-											</h4>
-
-											<p className="text-sm text-muted-foreground">
-												Recevez l&lsquo;intégralité des
-												notifications concernant la
-												gestion de vos fichiers par des
-												tiers ou des avertissements de
-												sécurité.
-											</p>
-										</div>
-									</Button>
-
-									<Button
-										{...field}
-										type="button"
-										variant={
-											field.value === "necessary"
-												? "secondary"
-												: "ghost"
-										}
-										onClick={() =>
-										{
-											form.setValue( "level", "necessary" );
-											setPush( true );
-										}}
-										disabled={isLoading}
-										className="-mx-2 h-auto justify-normal gap-4 p-3 text-left"
-									>
-										<BellMinus className="max-sm:hidden" />
-
-										<div>
-											<h4 className="mb-2 text-sm font-medium leading-none sm:mb-1">
-												Seulement les notifications
-												nécessaires
-											</h4>
-
-											<p className="text-sm text-muted-foreground">
-												Recevez seulement les
-												notifications importantes
-												concernant la gestion de vos
-												fichiers par des tiers.
-											</p>
-										</div>
-									</Button>
-
-									<Button
-										{...field}
-										type="button"
-										variant={
-											field.value === "off"
-												? "secondary"
-												: "ghost"
-										}
-										onClick={() =>
-										{
-											form.setValue( "level", "off" );
-											form.setValue( "push", false );
-											setPush( false );
-										}}
-										disabled={isLoading}
-										className="-mx-2 h-auto justify-normal gap-4 p-3 text-left"
-									>
-										<BellOff className="max-sm:hidden" />
-
-										<div>
-											<h4 className="mb-2 text-sm font-medium leading-none sm:mb-1">
-												Aucune notification
-											</h4>
-
-											<p className="text-sm text-muted-foreground">
-												Ne recevez aucune notification
-												sauf les alertes de sécurité
-												importantes.
-											</p>
-										</div>
-									</Button>
+									<p className="text-sm text-muted-foreground">
+										Recevez l&lsquo;intégralité des
+										notifications concernant la gestion de
+										votre compte utilisateur, de vos
+										fichiers par vous ou des tiers, ainsi
+										que les alertes de sécurité relatives au
+										site Internet et à votre compte.
+									</p>
 								</div>
-							</FormControl>
+
+								<Switch
+									checked={field.value === "all"}
+									disabled={isLoading}
+									className="ml-auto"
+									onCheckedChange={( checked ) =>
+									{
+										form.setValue(
+											"level",
+											checked ? "all" : level
+										);
+
+										setPush( true );
+									}}
+								/>
+							</div>
+
+							{/* Notifications nécessaires */}
+							<div className="flex items-center gap-4">
+								<BellMinus className="min-w-fit max-sm:hidden" />
+
+								<div>
+									<h4 className="mb-2 text-sm font-medium leading-4 sm:mb-1">
+										Seulement les notifications nécessaires
+									</h4>
+
+									<p className="text-sm text-muted-foreground">
+										Recevez seulement les notifications
+										importantes et nécessaires concernant la
+										gestion de votre compte utilisateur, les
+										fichiers partagés avec vous et les
+										alertes de sécurité critiques.
+									</p>
+								</div>
+
+								<Switch
+									checked={field.value === "necessary"}
+									disabled={isLoading}
+									className="ml-auto"
+									onCheckedChange={( checked ) =>
+									{
+										form.setValue(
+											"level",
+											checked ? "necessary" : level
+										);
+
+										setPush( true );
+									}}
+								/>
+							</div>
+
+							{/* Aucune notification */}
+							<div className="flex items-center gap-4">
+								<BellOff className="min-w-fit max-sm:hidden" />
+
+								<div>
+									<h4 className="mb-2 text-sm font-medium leading-4 sm:mb-1">
+										Aucune notification
+									</h4>
+
+									<p className="text-sm text-muted-foreground">
+										Ne recevez aucune notification sauf les
+										alertes de sécurité importantes
+										concernant votre compte utilisateur et
+										le site Internet.
+									</p>
+								</div>
+
+								<Switch
+									checked={field.value === "off"}
+									disabled={isLoading}
+									className="ml-auto"
+									onCheckedChange={( checked ) =>
+									{
+										form.setValue(
+											"level",
+											checked ? "off" : level
+										);
+										form.setValue( "push", false );
+
+										setPush( !checked );
+									}}
+								/>
+							</div>
 
 							<FormMessage />
 						</FormItem>
