eabd90fa6153da456bf09df5d0bc07ba3ce51190
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-03-09 11:57:03 +0100

    Removed `million` due to multiple issues with server components

--- next.config.js
+++ next.config.js
@@ -5,37 +5,33 @@
  */
 const { withSentryConfig } = require( "@sentry/nextjs" );
 const withNextIntl = require( "next-intl/plugin" )( "./utilities/i18n.ts" );
-const million = require( "million/compiler" );
 
-const nextConfig = million.next(
-	withNextIntl( {
-		poweredByHeader: false,
-		experimental: {
-			// https://github.com/getsentry/sentry-javascript/issues/10366
-			serverComponentsExternalPackages: ["@sentry/nextjs", "@sentry/node"],
-		},
-		basePath: "",
-		sentry: {
-			tunnelRoute: "/monitoring",
-			disableLogger: true,
-			hideSourceMaps: true,
-			widenClientFileUpload: true
-		},
-		async redirects()
-		{
-			return [
-				{
-					// Redirection de la page d'accueil des paramètres
-					//  vers l'onglet par défaut du profil utilisateur.
-					source: "/settings",
-					permanent: true,
-					destination: "/settings/user"
-				}
-			];
-		}
-	} ),
-	{ auto: { rsc: true } }
-);
+const nextConfig = withNextIntl( {
+	poweredByHeader: false,
+	experimental: {
+		// https://github.com/getsentry/sentry-javascript/issues/10366
+		serverComponentsExternalPackages: ["@sentry/nextjs", "@sentry/node"],
+	},
+	basePath: "",
+	sentry: {
+		tunnelRoute: "/monitoring",
+		disableLogger: true,
+		hideSourceMaps: true,
+		widenClientFileUpload: true
+	},
+	async redirects()
+	{
+		return [
+			{
+				// Redirection de la page d'accueil des paramètres
+				//  vers l'onglet par défaut du profil utilisateur.
+				source: "/settings",
+				permanent: true,
+				destination: "/settings/user"
+			}
+		];
+	}
+} );
 
 const sentryConfig = {
 	org: process.env.SENTRY_ORG,
