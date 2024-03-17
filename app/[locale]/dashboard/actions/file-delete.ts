705bee563e4267603c9fcbe808232030db51342e
Florian Trayon <git@florian-trayon.fr>
2024-03-17 17:34:41 +0100

    Moved all server actions for user file processing to separate files

---
+++ app/[locale]/dashboard/actions/file-delete.ts
@@ -0,0 +1,126 @@
+//
+// Action de suppression d'un ou plusieurs fichiers.
+//
+
+"use server";
+
+import { z } from "zod";
+import prisma from "@/utilities/prisma";
+import { join } from "path";
+import { auth } from "@/utilities/next-auth";
+import * as Sentry from "@sentry/nextjs";
+import { existsSync } from "fs";
+import { rm, readdir } from "fs/promises";
+
+export async function deleteFile( formData: FormData )
+{
+	// On récupère d'abord la session de l'utilisateur.
+	const session = await auth();
+
+	if ( !session )
+	{
+		return [];
+	}
+
+	// On créé ensuite un schéma de validation personnalisé pour
+	//  les données du formulaire.
+	const validation = z.object( {
+		fileIds: z.array( z.string().uuid() )
+	} );
+
+	// On tente alors de valider les données du formulaire.
+	const result = validation.safeParse( {
+		fileIds: formData.getAll( "fileId" )
+	} );
+
+	if ( !result.success )
+	{
+		return [];
+	}
+
+	// On récupère avant de supprimer après les fichiers dans la base
+	//  de données pour vérifier si l'opération a réussi.
+	const query = {
+		id: {
+			in: result.data.fileIds
+		},
+		OR: [
+			{
+				userId: session.user.id
+			},
+			{
+				shares: {
+					some: {
+						userId: session.user.id,
+						status: "write"
+					}
+				}
+			}
+		]
+	};
+
+	const [ files ] = await prisma.$transaction( [
+		prisma.file.findMany( {
+			select: {
+				id: true,
+				userId: true
+			},
+			where: query
+		} ),
+
+		prisma.file.deleteMany( { where: query } )
+	] );
+
+	if ( files.length === 0 )
+	{
+		// Si aucun fichier n'a été trouvé dans la base de données,
+		//  on retourne une valeur d'échec.
+		return [];
+	}
+
+	try
+	{
+		// On parcourt l'ensemble des fichiers à supprimer.
+		await Promise.all(
+			files.map( async ( file ) =>
+			{
+				// On supprime le fichier et le dossier associé dans le
+				//  système de fichiers.
+				const userFolder = join(
+					process.cwd(),
+					"public/files",
+					file.userId
+				);
+
+				if ( existsSync( userFolder ) )
+				{
+					// On supprime le dossier où se trouve les versions du
+					//  fichier.
+					const fileFolder = join( userFolder, file.id );
+
+					if ( existsSync( fileFolder ) )
+					{
+						await rm( fileFolder, { recursive: true, force: true } );
+					}
+
+					// On supprime de la même manière le dossier de l'utilisateur
+					//  si celui-ci est vide après la suppression du fichier.
+					if ( ( await readdir( userFolder ) ).length === 0 )
+					{
+						await rm( userFolder, { recursive: true, force: true } );
+					}
+				}
+			} )
+		);
+	}
+	catch ( error )
+	{
+		// Si une erreur s'est produite lors des opérations avec le
+		//  système de fichiers, on l'envoie tout simplement à Sentry.
+		Sentry.captureException( error );
+	}
+
+	// On retourne enfin la liste des identifiants des fichiers supprimés
+	//  à la fin du traitement.
+	return files.map( ( file ) => file.id );
+}
