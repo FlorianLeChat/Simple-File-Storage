94625387f79d2717818c58f85de50bb95e3b2f88
Florian Trayon <git@florian-trayon.fr>
2024-03-17 20:40:11 +0100

    Moved dual-factor authentication modal to dedicated components

--- app/[locale]/settings/components/user.tsx
+++ app/[locale]/settings/components/user.tsx
@@ -4,22 +4,19 @@
 
 "use client";
 
-import Image from "next/image";
 import * as z from "zod";
 import schema from "@/schemas/user";
 import { toast } from "sonner";
-import { merge } from "@/utilities/tailwind";
-import { useForm } from "react-hook-form";
 import { Lock,
-	Check,
 	AtSign,
 	Contact,
 	Loader2,
 	RefreshCw,
 	FileImage,
 	Languages,
-	Smartphone,
-	ClipboardCopy } from "lucide-react";
+	Smartphone } from "lucide-react";
+import { merge } from "@/utilities/tailwind";
+import { useForm } from "react-hook-form";
 import serverAction from "@/utilities/recaptcha";
 import { useLocale } from "next-intl";
 import { languages } from "@/config/languages";
@@ -50,13 +47,7 @@ import { Form,
 	FormMessage,
 	FormDescription } from "../../components/ui/form";
 import { updateUser } from "../actions/update-user";
-import { validateOTP } from "../actions/validate-otp";
-import { Dialog,
-	DialogClose,
-	DialogTitle,
-	DialogHeader,
-	DialogContent,
-	DialogDescription } from "../../components/ui/dialog";
+import OTPValidationModal from "./otp-validation";
 import { Button, buttonVariants } from "../../components/ui/button";
 
 export default function User( {
@@ -70,10 +61,8 @@ export default function User( {
 } )
 {
 	// Déclaration des variables d'état.
-	const [ showOTP, setShowOTP ] = useState( false );
 	const [ isLocked, setLocked ] = useState( false );
 	const [ isLoading, setLoading ] = useState( false );
-	const [ backupCode, setBackupCode ] = useState( "" );
 	const [ updateState, updateAction ] = useFormState( updateUser, {
 		success: true,
 		reason: ""
@@ -120,47 +109,6 @@ export default function User( {
 			( previous, current ) => previous + characters[ current % characters.length ],
 			""
 		);
-	};
-
-	// Soumission de la requête de validation de la double authentification.
-	const submitOTPValidation = async ( code: string ) =>
-	{
-		// Activation de l'état de chargement.
-		setLoading( true );
-
-		// Récupération des données du formulaire.
-		const formData = new FormData();
-		formData.set( "secret", secret );
-		formData.set( "code", code );
-
-		// Envoi de la requête au serveur et
-		//  traitement de la réponse.
-		const state = await serverAction( validateOTP, formData );
-
-		// Fin de l'état de chargement.
-		setLoading( false );
-
-		if ( state !== false && state !== "" )
-		{
-			// Fermeture de la boîte de dialogue courante.
-			setShowOTP( false );
-
-			// Ouverture de la boîte de dialogue pour le
-			//  code de secours.
-			setBackupCode( state as string );
-
-			// Envoi d'une notification de succès.
-			toast.success( "form.info.action_success", {
-				description: "form.info.otp_enabled"
-			} );
-		}
-		else
-		{
-			// Envoi d'une notification d'erreur.
-			toast.error( "form.errors.file_deleted", {
-				description: "form.errors.server_error"
-			} );
-		}
 	};
 
 	// Détection de la response du serveur après l'envoi du formulaire.
@@ -386,16 +334,10 @@ export default function User( {
 									d&lsquo;authentification envoyés par
 									courriel.{" "}
 									{!session.user.otp && (
-										<Button
-											type="button"
-											variant="link"
-											onClick={() => setShowOTP( true )}
-											className="h-auto p-0 text-muted-foreground underline decoration-dotted underline-offset-4 dark:hover:text-foreground"
-										>
-											Activation de
-											l&lsquo;authentification à deux
-											facteurs
-										</Button>
+										<OTPValidationModal
+											image={image}
+											secret={secret}
+										/>
 									)}
 								</FormDescription>
 
@@ -412,116 +354,6 @@ export default function User( {
 						passe.
 					</p>
 				)}
-
-				{/* Procédure de validation de la double authentification */}
-				<Dialog open={showOTP} onOpenChange={setShowOTP}>
-					<DialogContent className="h-fit max-h-[calc(100%-2rem)] overflow-auto max-sm:max-w-[calc(100%-2rem)] md:max-h-[50%]">
-						<DialogHeader>
-							<DialogTitle className="flex items-center">
-								<Smartphone className="mr-2 inline h-5 w-5" />
-								Authentification à deux facteurs
-							</DialogTitle>
-
-							<Image
-								src={image}
-								alt="QR Code"
-								width={164}
-								height={164}
-								className="!mb-2 !mt-4"
-							/>
-
-							<DialogDescription className="text-left">
-								Afin de sécuriser davantage votre compte, vous
-								pouvez activer l&lsquo;authentification à deux
-								facteurs.
-								<br />
-								<br />
-								1. Tout d&lsquo;abord, vous devez scanner le
-								code QR ci-dessus avec une application
-								compatible comme{" "}
-								<a
-									rel="noopener noreferrer"
-									href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2"
-									target="_blank"
-									className="underline decoration-dotted underline-offset-4 dark:hover:text-foreground"
-								>
-									Google Authenticator
-								</a>{" "}
-								ou{" "}
-								<a
-									rel="noopener noreferrer"
-									href="https://play.google.com/store/apps/details?id=com.authy.authy"
-									target="_blank"
-									className="underline decoration-dotted underline-offset-4 dark:hover:text-foreground"
-								>
-									Authy
-								</a>
-								.
-								<br />
-								<br />
-								2. Une fois activée, veuillez saisir ci-dessous
-								le code de sécurité généré par votre application
-								pour valider la double authentification.
-								<br />
-								<br />
-								3. Lorsque la double authentification est
-								validée, vous serez invité à sauvegarder un code
-								de secours pour désactiver la double
-								authentification si vous perdez votre téléphone.
-							</DialogDescription>
-						</DialogHeader>
-
-						<form
-							id="validate-otp-form"
-							onSubmit={( event ) =>
-							{
-								// Arrêt du comportement par défaut.
-								event.preventDefault();
-
-								// Récupération de la valeur du champ de saisie.
-								const element = event.currentTarget.children[ 0 ]
-									.children[ 1 ] as HTMLInputElement;
-
-								submitOTPValidation( element.value );
-							}}
-						>
-							<InputOTP
-								name="code"
-								render={( { slots } ) => (
-									<InputOTPGroup>
-										{slots.map( ( slot, index ) => (
-											<InputOTPSlot
-												{...slot}
-												key={index}
-											/>
-										) )}
-									</InputOTPGroup>
-								)}
-								maxLength={6}
-							/>
-						</form>
-
-						<Button
-							type="submit"
-							form="validate-otp-form"
-							disabled={isLoading}
-							className="w-full"
-						>
-							{isLoading ? (
-								<>
-									<Loader2 className="mr-2 h-4 w-4 animate-spin" />
-									Veuillez patienter...
-								</>
-							) : (
-								<>
-									<Check className="mr-2 h-4 w-4" />
-									Activer l&lsquo;authentification à deux
-									facteurs
-								</>
-							)}
-						</Button>
-					</DialogContent>
-				</Dialog>
 
 				{/* Authentification à deux facteurs */}
 				{session.user.otp && (
@@ -569,51 +401,6 @@ export default function User( {
 						)}
 					/>
 				)}
-
-				{/* Code de secours */}
-				<Dialog open={backupCode !== ""}>
-					<DialogContent className="h-fit max-h-[calc(100%-2rem)] overflow-auto max-sm:max-w-[calc(100%-2rem)] md:max-h-[50%]">
-						<DialogHeader>
-							<DialogTitle className="flex items-center">
-								<Smartphone className="mr-2 inline h-5 w-5" />
-								Code de secours
-							</DialogTitle>
-
-							<DialogDescription className="text-left">
-								Votre compte est désormais protégé par
-								l&lsquo;authentification à deux facteurs.
-								Ci-dessous se trouve un code de secours que vous
-								pouvez utiliser afin de désactiver
-								l&lsquo;authentification à deux facteurs si vous
-								perdez votre téléphone.{" "}
-								<strong>
-									Si vous perdez ce code, vous devrez
-									contacter le support technique mais votre
-									demande pourra être refusée en cas de
-									négligence constatée.
-								</strong>
-								<br />
-								<br />
-								<code>{backupCode}</code>
-							</DialogDescription>
-						</DialogHeader>
-
-						<DialogClose
-							onClick={() =>
-							{
-								// Copie dans le presse-papiers.
-								navigator.clipboard.writeText( backupCode );
-
-								// Réinitialisation du code de secours.
-								setBackupCode( "" );
-							}}
-							className={merge( buttonVariants(), "w-full" )}
-						>
-							<ClipboardCopy className="mr-2 h-4 w-4" />
-							Copier dans le presse-papiers
-						</DialogClose>
-					</DialogContent>
-				</Dialog>
 
 				{/* Langue préférée */}
 				<FormField
