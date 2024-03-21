696120ead1c123d1eeb0e0967ed0c1fa0d432533
Florian Trayon <git@florian-trayon.fr>
2024-03-21 21:41:46 +0100

    Added translations for server actions and authentication components

--- app/[locale]/authentication/components/reset-password.tsx
+++ app/[locale]/authentication/components/reset-password.tsx
@@ -5,6 +5,7 @@
 "use client";
 
 import { merge } from "@/utilities/tailwind";
+import { useTranslations } from "next-intl";
 import { Check, ShieldQuestion } from "lucide-react";
 
 import { Dialog,
@@ -18,6 +19,9 @@ import { buttonVariants } from "../../components/ui/button";
 
 export default function ResetPasswordModal()
 {
+	// Déclaration des variables d'état.
+	const t = useTranslations( "modals.reset_password" );
+
 	// Affichage du rendu HTML du composant.
 	return (
 		<Dialog>
@@ -29,36 +33,30 @@ export default function ResetPasswordModal()
 					"h-auto p-0 text-muted-foreground underline decoration-dotted underline-offset-4 dark:hover:text-foreground"
 				)}
 			>
-				Vous avez oublié votre mot de passe ?
+				{t( "trigger" )}
 			</DialogTrigger>
 
 			<DialogContent className="h-fit max-h-[calc(100%-2rem)] overflow-auto max-sm:max-w-[calc(100%-2rem)] md:max-h-[50%]">
 				<DialogHeader>
 					<DialogTitle className="flex items-center">
 						<ShieldQuestion className="mr-2 inline h-5 w-5" />
-						Oubli du mot de passe
+						{t( "title" )}
 					</DialogTitle>
 
 					<DialogDescription className="text-left">
-						Si vous avez oublié votre mot de passe, essayez de vous
-						connecter à votre compte avec un lien
-						d&lsquo;authentification à usage unique envoyé à votre
-						adresse électronique. Une fois connecté, vous pourrez
-						saisir un nouveau mot de passe pour votre compte.
-						<br />
-						<strong className="mt-1 inline-block">
-							En cas de perte de l&lsquo;accès à votre adresse
-							électronique, vous devrez recréer un nouveau compte
-							car le support technique ne sera pas légalement
-							autorisé à vous aider à récupérer l&lsquo;accès à
-							votre compte.
-						</strong>
+						{t.rich( "description", {
+							strong: ( chunks ) => (
+								<strong className="mt-1 inline-block">
+									{chunks}
+								</strong>
+							)
+						} )}
 					</DialogDescription>
 				</DialogHeader>
 
 				<DialogClose className={merge( buttonVariants(), "w-full" )}>
 					<Check className="mr-2 h-4 w-4" />
-					J&lsquo;ai bien compris
+					{t( "close" )}
 				</DialogClose>
 			</DialogContent>
 		</Dialog>
