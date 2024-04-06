f5d5b796cc9c28e35659991c97a5d6f25b7c667a
Florian Trayon <git@florian-trayon.fr>
2024-04-06 14:47:20 +0200

    Added CLI script to generate fake user accounts for e2e tests

---
+++ scripts/create-fake-account.ts
@@ -0,0 +1,35 @@
+//
+// Création de comptes utilisateurs factices pour les tests e2e.
+//
+
+// Importation des dépendances.
+import { join } from "path";
+import { faker } from "@faker-js/faker";
+import * as dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+// Configuration des variables d'environnement.
+dotenv.config( { path: join( process.cwd(), ".env.local" ), override: true } );
+
+// Exécution de la fonction asynchrone.
+const script = async () =>
+{
+	// Création d'une nouvelle instance de Prisma et création
+	//  d'un compte utilisateur factice.
+	const prisma = new PrismaClient();
+
+	// Génération de 5 comptes utilisateurs factices.
+	await prisma.user.createMany( {
+		data: Array.from( [ 1, 2, 3, 4, 5 ], ( index ) => ( {
+			name: faker.person.fullName(),
+			email: `test${ index }@gmail.com`,
+			password:
+				"$2a$15$ztEyG.IzUf5mIO9kynUeoOZarLtxIRY7o.Eynzc501/MsrG6.DjGW"
+		} ) )
+	} );
+
+	// Déconnexion de la base de données.
+	prisma.$disconnect();
+};
+
+script();
