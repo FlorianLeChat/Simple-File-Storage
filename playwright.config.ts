edf5c7a01f14b9b211768fde93d072f4ca86d118
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-04-24 19:59:08 +0200

    Reduced browsers used by PlayWright tests in GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,20 +28,8 @@ export default defineConfig( {
 			use: { ...devices[ "Desktop Chrome" ] }
 		},
 		{
-			name: "firefox",
-			use: { ...devices[ "Desktop Firefox" ] }
-		},
-		{
-			name: "webkit",
-			use: { ...devices[ "Desktop Safari" ] }
-		},
-		{
 			name: "Mobile Chrome",
 			use: { ...devices[ "Pixel 5" ] }
-		},
-		{
-			name: "Mobile Safari",
-			use: { ...devices[ "iPhone 12" ] }
 		}
 	]
 } as PlaywrightTestConfig );
