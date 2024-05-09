366312a81688c0fd719e39501b82daca7f03b7de
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-05-09 17:05:51 +0200

    Added e2e tests to manage notifications following user actions

--- tests/e2e/notifications.spec.ts
+++ tests/e2e/notifications.spec.ts
@@ -1,3 +1,4 @@
+import { join } from "path";
 import { execSync } from "child_process";
 import { test, expect } from "@playwright/test";
 
@@ -6,38 +7,127 @@ import { test, expect } from "@playwright/test";
 //
 test( "Vérification du contrôle des notifications", async ( { page } ) =>
 {
-	// Ce test est désactivé car il ne fonctionne pas sur les navigateurs mobiles.
-	test.skip();
-
 	// Réinitialisation des comptes utilisateurs factices.
 	execSync( "node scripts/create-fake-accounts.js" );
 
-	// Accès à la page d'authentification.
+	// Authentification et accès au tableau de bord.
 	await page.goto( "/authentication" );
-
-	// Clic sur l'onglet « Connexion ».
 	await page.getByRole( "tab", { name: "Login" } ).click();
-
-	// Remplissage des champs de saisie de l'adresse électronique et du mot de passe.
 	await page.getByPlaceholder( "name@domain.com" ).fill( "test1@gmail.com" );
 	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4016" );
-
-	// Clic sur le bouton de connexion.
 	await page.getByText( "Log in by password" ).click();
-
-	// Attente de la redirection vers la page du tableau de bord.
 	await expect( page ).toHaveURL( "/dashboard" );
 
-	// Première mise à jour des informations du compte utilisateur.
-	//  Note : une notification est envoyée pour indiquer que le mot de passe a été modifié.
-	await page.goto( "/settings/user" );
-	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4017" );
-	await page.getByRole( "button", { name: "Update" } ).click();
+	// Téléversement de deux fichiers quelconques.
+	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
+	await page
+		.getByRole( "textbox", { name: "File Upload" } )
+		.setInputFiles( [
+			join( __dirname, "static/cat.jpg" ),
+			join( __dirname, "static/duck.jpg" )
+		] );
+	await page.getByRole( "button", { name: "Upload" } ).click();
 	await expect(
 		page.locator( "[data-sonner-toast][data-type = success]" )
 	).toHaveCount( 1 );
 
-	// Mise à jour des préférences de notification.
+	// Ajout d'un utilisateur en partage en écriture sur le
+	//  premier fichier téléversé.
+	await page
+		.getByRole( "button", { name: "Open action menu" } )
+		.first()
+		.click();
+	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
+	await page.getByPlaceholder( "Search..." ).fill( "test2@gmail.com" );
+	await page
+		.locator( "li" )
+		.filter( { hasText: "test2@gmail.com" } )
+		.getByRole( "button" )
+		.click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+	await page.getByRole( "combobox" ).click();
+	await page.getByLabel( "Write" ).click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+
+	// Rechargement de la page pour fermer le menu des actions.
+	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
+	await page.reload();
+
+	// Ajout d'un utilisateur en partage en écriture sur le
+	//  deuxième fichier téléversé.
+	await page.getByRole( "button", { name: "Open action menu" } ).last().click();
+	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
+	await page.getByPlaceholder( "Search..." ).fill( "test2@gmail.com" );
+	await page
+		.locator( "li" )
+		.filter( { hasText: "test2@gmail.com" } )
+		.getByRole( "button" )
+		.click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+	await page.getByRole( "combobox" ).click();
+	await page.getByLabel( "Write" ).click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+
+	// Rechargement de la page pour fermer le menu des actions.
+	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
+	await page.reload();
+
+	// Déconnexion du compte utilisateur actuel.
+	await page.locator( "header aside button:last-of-type" ).click();
+	await page.getByText( "Logout" ).click();
+	await expect( page ).toHaveURL( "/" );
+
+	// Connexion avec le compte utilisateur partagé en écriture.
+	await page.goto( "/authentication" );
+	await page.getByRole( "tab", { name: "Login" } ).click();
+	await page.getByPlaceholder( "name@domain.com" ).fill( "test2@gmail.com" );
+	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4016" );
+	await page.getByText( "Log in by password" ).click();
+	await expect( page ).toHaveURL( "/dashboard" );
+
+	// Réception des notifications et marquage de toutes comme lues.
+	await page.locator( ".lucide-bell-ring" ).click();
+	await expect( page.getByText( "Folder Sharing" ) ).toHaveCount( 2 );
+	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+
+	// Suppression du partage en écriture du premier fichier partagé.
+	await page.goto( "/dashboard" );
+	await page
+		.getByRole( "button", { name: "Open action menu" } )
+		.first()
+		.click();
+	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
+	await page.getByRole( "button", { name: "Permanently Delete" } ).click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+
+	// Rechargement de la page pour fermer le menu des actions.
+	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
+	await page.reload();
+
+	// Réception des notifications et vérification de la notification
+	//  de suppression du partage.
+	await expect( page ).toHaveURL( "/dashboard" );
+	await page.locator( ".lucide-bell-ring" ).click();
+	await expect( page.getByText( "Deletion of a Shared File" ) ).toHaveCount( 1 );
+	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
+	await expect(
+		page.locator( "[data-sonner-toast][data-type = success]" )
+	).toHaveCount( 1 );
+
+	// Mise à jour des préférences de notification (désactivation totale).
 	await page.goto( "/settings/notifications" );
 	await page.getByRole( "switch" ).last().click();
 	await page.getByRole( "button", { name: "Update" } ).click();
@@ -45,26 +135,20 @@ test( "Vérification du contrôle des notifications", async ( { page } ) =>
 		page.locator( "[data-sonner-toast][data-type = success]" )
 	).toHaveCount( 1 );
 
-	// Réception des notifications et marquage de toutes comme lues.
-	await page.reload();
-	await page.locator( ".lucide-bell-ring" ).click();
-	await expect( page.getByText( "Password Change" ) ).toHaveCount( 1 );
-	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
+	// Suppression du partage en écriture du deuxième fichier partagé.
+	await page.goto( "/dashboard" );
+	await page.getByRole( "button", { name: "Open action menu" } ).click();
+	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
+	await page.getByRole( "button", { name: "Permanently Delete" } ).click();
 	await expect(
 		page.locator( "[data-sonner-toast][data-type = success]" )
 	).toHaveCount( 1 );
 
-	// Deuxième mise à jour des informations du compte utilisateur.
-	//  Note : la notification ne doit pas être envoyée cette fois-ci.
-	await page.goto( "/settings/user" );
-	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4018" );
-	await page.getByRole( "button", { name: "Update" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
+	// Rechargement de la page pour fermer le menu des actions.
+	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
+	await page.reload();
 
 	// Attente de la réception des notifications et vérification des notifications.
-	await page.reload();
 	await page.locator( ".lucide-bell-ring" ).click();
-	await expect( page.getByText( "Password Change" ) ).toHaveCount( 0 );
+	await expect( page.getByText( "Deletion of a Shared File" ) ).toHaveCount( 0 );
 } );
