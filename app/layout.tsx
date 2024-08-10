78d9ada182ea8499e39507107997e37fd1d63e5f
Florian Trayon <git@florian-trayon.fr>
2024-08-10 18:56:55 +0200

    Fixed global CSS and Google font importation from dynamic routes

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+//
+// Importation statique des règles CSS transformées par Tailwind CSS.
+//  Note : cette page ne devrait pas exister mais NextJS ne permet pas
+//   de charger les règles CSS globales depuis des routes dynamiques...
+//
+
+import "./layout.css";
+import { logger } from "@/utilities/pino";
+import type { ReactNode } from "react";
+import { Inter, Poppins, Roboto } from "next/font/google";
+
+const inter = Inter( {
+	subsets: [ "latin" ],
+	display: "swap"
+} );
+
+const poppins = Poppins( {
+	weight: [ "100", "200", "300", "400", "500", "600", "700", "800", "900" ],
+	subsets: [ "latin" ],
+	display: "swap"
+} );
+
+const roboto = Roboto( {
+	weight: [ "100", "300", "400", "500", "700", "900" ],
+	subsets: [ "latin" ],
+	display: "swap"
+} );
+
+export default function Layout( { children }: { children: ReactNode } )
+{
+	// Les polices de caractères sont chargées mais ne sont pas utilisées
+	//  dans ce fichier, elles sont ainsi utilisées dans les routes dynamiques
+	//  en appelant de nouveau ces mêmes fonctions car elles sont désormais en cache.
+	logger.debug(
+		{ source: __filename, fonts: [ inter, poppins, roboto ] },
+		"Fonts loaded"
+	);
+
+	return children;
+}
