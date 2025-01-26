a33d76c6a6ccd13ea1be43ebe61a03f23cccc7cb
Florian Trayon <git@florian-trayon.fr>
2025-01-26 12:48:06 +0100

    Added error capture for nested React Server components by Sentry

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,19 +2,22 @@
 // Ajout la gestion des erreurs et de suivi des performances côté serveur avec Sentry.
 //  Source : https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#create-initialization-config-files
 //
-import { init } from "@sentry/nextjs";
+import { init, captureRequestError } from "@sentry/nextjs";
+
+export const onRequestError = captureRequestError;
 
 export async function register()
 {
-	if (
-		process.env.NEXT_RUNTIME === "nodejs"
-		|| process.env.NEXT_RUNTIME === "edge"
-	)
+	const runtime = process.env.NEXT_RUNTIME;
+	const isNodeRuntime = runtime === "nodejs";
+	const isEdgeRuntime = runtime === "edge";
+	const isDevelopment = process.env.NODE_ENV === "development";
+
+	if ( isNodeRuntime || isEdgeRuntime )
 	{
 		init( {
 			dsn: process.env.SENTRY_DSN,
-			tracesSampleRate:
-				process.env.NODE_ENV === "development" ? 1.0 : 0.01
+			tracesSampleRate: isDevelopment ? 1.0 : 0.01
 		} );
 	}
 }
