d2910fcce11c56da0dbef3d142565fba5690acae
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-02-05 18:46:54 +0100

    Added support for Gravatr URLs on non-OAuth 2.0 user accounts

---
+++ utilities/gravatar.ts
@@ -0,0 +1,15 @@
+//
+// Récupère l'URL Gravatar d'une adresse électronique quelconque.
+//  Source : https://docs.gravatar.com/api/avatars/node/
+//
+export const getGravatarUrl = async ( email: string, size: number = 64 ) =>
+{
+	const messageBuffer = new TextEncoder().encode( email.trim().toLowerCase() );
+	const hashBuffer = await crypto.subtle.digest( "SHA-256", messageBuffer );
+	const hashArray = Array.from( new Uint8Array( hashBuffer ) );
+	const hashHex = hashArray
+		.map( ( b ) => b.toString( 16 ).padStart( 2, "0" ) )
+		.join( "" );
+
+	return `https://www.gravatar.com/avatar/${ hashHex }?s=${ size }&d=identicon`;
+};
