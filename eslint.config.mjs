d702b9f2d0a7e2bb391915fc959ad8be525ae0f6
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-03-01 10:14:48 +0100

    Removed redundant legacy configuration settings before ESLint 9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,7 @@ import tslint from "typescript-eslint";
 import globals from "globals";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import tailwind from "eslint-plugin-tailwindcss";
-import tsParser from "@typescript-eslint/parser";
 import stylistic from "@stylistic/eslint-plugin";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 
 export default tslint.config(
 	{
@@ -33,17 +31,13 @@ export default tslint.config(
 	},
 	{
 		plugins: {
-			"@stylistic": stylistic,
-			"@typescript-eslint": typescriptEslint
+			"@stylistic": stylistic
 		},
 		languageOptions: {
-			parser: tsParser,
 			globals: {
 				...globals.node,
 				...globals.browser
 			},
-			sourceType: "script",
-			ecmaVersion: "latest",
 			parserOptions: {
 				project: [ "./tsconfig.json" ]
 			}
