4abaceb2b4ca223546c619b03229a66cbdac835b
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-05-10 20:57:58 +0200

    Improved starting conditions for critical Docker services

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -3,9 +3,6 @@
 # Generate random secret for JWT
 sed -i "s#AUTH_SECRET=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#AUTH_SECRET=$(openssl rand -base64 32)#g" .env
 
-# Wait for external services to be ready
-/wait
-
 # Execute database migrations
 npm run migrate
 
