da2ebe100c02cf1368dc8b3c2dce3700d00594dc
Florian Trayon <git@florian-trayon.fr>
2026-04-05 17:14:41 +0200

    fix(next): use native URL object API for sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,38 +1,25 @@
-//
-// Route vers le fichier du plan du site.
-//  Source : https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
-//
 import { fetchMetadata } from "@/utilities/metadata";
 
 export default async function Sitemap()
 {
-	// Déclaration des constantes.
 	const date = new Date();
 	const baseUrl = new URL( ( await fetchMetadata() )?.metadataBase ?? "" );
-	const pathname = baseUrl.pathname.endsWith( "/" )
-		? baseUrl.pathname
-		: `${ baseUrl.pathname }/`;
 
-	// Génération du plan du site.
 	return [
 		{
-			// Page d'accueil.
 			url: baseUrl,
 			lastModified: date
 		},
 		{
-			// Page d'authentification.
-			url: new URL( `${ pathname }authentication`, baseUrl ),
+			url: baseUrl + "authentication",
 			lastModified: date
 		},
 		{
-			// Conditions d'utilisation.
-			url: new URL( `${ pathname }legal/terms`, baseUrl ),
+			url: baseUrl + "legal/terms",
 			lastModified: date
 		},
 		{
-			// Politique de confidentialité.
-			url: new URL( `${ pathname }legal/privacy`, baseUrl ),
+			url: baseUrl + "legal/privacy",
 			lastModified: date
 		}
 	];
