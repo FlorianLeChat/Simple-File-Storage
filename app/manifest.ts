62322e5567a64195b1ec0b3fc2547ba13e533f53
Florian Trayon <git@florian-trayon.fr>
2026-04-06 10:29:14 +0200

    refactor(next): improve usage of absolute paths for metadata and manifest generation

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,52 +1,46 @@
-//
-// Route vers le manifeste de l'application.
-//  Source : https://nextjs.org/docs/app/api-reference/file-conventions/metadata/manifest
-//
 import { fetchMetadata } from "@/utilities/metadata";
 import { type MetadataRoute } from "next";
 
 export default async function manifest(): Promise<MetadataRoute.Manifest>
 {
-	// Déclaration des constantes.
 	const metadata = await fetchMetadata();
 
-	// Génération du manifeste.
 	return {
 		name: metadata.title as string,
 		display: "standalone",
 		start_url: "/",
 		short_name: metadata.title as string,
 		description: metadata.description as string,
-		theme_color: "#3b82f6", // Couleur par défaut du thème sombre (en bleu).
-		background_color: "#020817", // Couleur par défaut de l'arrière-plan du thème sombre (en bleu).
+		theme_color: "#3b82f6",
+		background_color: "#020817",
 		icons: [
 			{
-				src: "assets/favicons/16x16.webp",
+				src: "/assets/favicons/16x16.webp",
 				type: "image/webp",
 				sizes: "16x16"
 			},
 			{
-				src: "assets/favicons/32x32.webp",
+				src: "/assets/favicons/32x32.webp",
 				type: "image/webp",
 				sizes: "32x32"
 			},
 			{
-				src: "assets/favicons/48x48.webp",
+				src: "/assets/favicons/48x48.webp",
 				type: "image/webp",
 				sizes: "48x48"
 			},
 			{
-				src: "assets/favicons/180x180.webp",
+				src: "/assets/favicons/180x180.webp",
 				type: "image/webp",
 				sizes: "180x180"
 			},
 			{
-				src: "assets/favicons/192x192.webp",
+				src: "/assets/favicons/192x192.webp",
 				type: "image/webp",
 				sizes: "192x192"
 			},
 			{
-				src: "assets/favicons/512x512.webp",
+				src: "/assets/favicons/512x512.webp",
 				type: "image/webp",
 				sizes: "512x512"
 			}
--- utilities/metadata.ts
+++ utilities/metadata.ts
@@ -1,6 +1,3 @@
-//
-// Génération des métadonnées du site Internet à partir de l'API GitHub.
-//
 import { join } from "node:path";
 import { logger } from "@/utilities/pino";
 import { existsSync } from "node:fs";
@@ -9,7 +6,6 @@ import { mkdir, readFile, writeFile } from "node:fs/promises";
 
 const fetchWithCache = async ( url: string ) =>
 {
-	// Récupération des données avec mise en cache forcée.
 	const response = await fetch( url, { cache: "force-cache" } );
 
 	return response.json() as Promise<Record<string, string>>;
@@ -17,8 +13,6 @@ const fetchWithCache = async ( url: string ) =>
 
 export async function fetchMetadata(): Promise<Metadata & { source: string }>
 {
-	// On vérifie d'abord si les métadonnées sont déjà enregistrées
-	//  dans le cache du système de fichiers.
 	const folderPath = join( process.cwd(), "data" );
 	const filePath = join( folderPath, "metadata.json" );
 
@@ -31,8 +25,6 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
 		};
 	}
 
-	// On récupère ensuite les informations du dépôt GitHub,
-	//  ceux de l'auteur et le dernier commit.
 	const repository = await fetchWithCache(
 		"https://api.github.com/repos/FlorianLeChat/Simple-File-Storage"
 	);
@@ -45,63 +37,55 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
 		"https://api.github.com/repos/FlorianLeChat/Simple-File-Storage/commits/master"
 	);
 
-	// On détermine après certaines métadonnées récurrentes.
 	const banner = `https://opengraph.githubassets.com/${ commits.sha }/${ repository.full_name }`;
 	const title = repository.name.replaceAll( "-", " " );
 	const url = process.env.NEXT_PUBLIC_ENV === "production"
 		? repository.homepage
 		: `http://localhost:${ process.env.PORT ?? 3000 }`;
 
-	// On retourne également les métadonnées récupérées récemment
-	//  avant de les enregistrer dans un fichier JSON.
 	const metadata = {
-		// Métadonnées du document.
 		title,
 		source: repository.html_url,
 		authors: [ { name: author.name, url: author.html_url } ],
 		keywords: repository.topics,
 		description: repository.description,
 		metadataBase: new URL( url ),
-
-		// Icônes du document.
 		icons: {
 			icon: [
 				{
-					url: "assets/favicons/16x16.webp",
+					url: "/assets/favicons/16x16.webp",
 					type: "image/webp",
 					sizes: "16x16"
 				},
 				{
-					url: "assets/favicons/32x32.webp",
+					url: "/assets/favicons/32x32.webp",
 					type: "image/webp",
 					sizes: "32x32"
 				},
 				{
-					url: "assets/favicons/48x48.webp",
+					url: "/assets/favicons/48x48.webp",
 					type: "image/webp",
 					sizes: "48x48"
 				},
 				{
-					url: "assets/favicons/192x192.webp",
+					url: "/assets/favicons/192x192.webp",
 					type: "image/webp",
 					sizes: "192x192"
 				},
 				{
-					url: "assets/favicons/512x512.webp",
+					url: "/assets/favicons/512x512.webp",
 					type: "image/webp",
 					sizes: "512x512"
 				}
 			],
 			apple: [
 				{
-					url: "assets/favicons/180x180.webp",
+					url: "/assets/favicons/180x180.webp",
 					type: "image/webp",
 					sizes: "180x180"
 				}
 			]
 		},
-
-		// Informations pour les moteurs de recherche.
 		openGraph: {
 			url,
 			type: "website",
@@ -113,8 +97,6 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
 				}
 			]
 		},
-
-		// Informations pour la plate-forme Twitter.
 		twitter: {
 			title,
 			creator: `@${ author.twitter_username }`,
@@ -127,8 +109,6 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
 		}
 	};
 
-	// On enregistre enfin les métadonnées dans un fichier JSON
-	//  avant de les retourner.
 	logger.warn(
 		{ source: __dirname, metadata },
 		"Loading metadata from GitHub API"
