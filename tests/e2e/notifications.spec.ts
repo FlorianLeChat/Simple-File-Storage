c85c576df158786b613c3ee208290ce8b4431ba1
Florian Trayon <git@florian-trayon.fr>
2026-05-09 15:19:09 +0200

    test(e2e): run Playwright e2e tests before build step

--- tests/e2e/notifications.spec.ts
+++ tests/e2e/notifications.spec.ts
@@ -2,156 +2,109 @@ import { join } from "path";
 import { execSync } from "child_process";
 import { test, expect } from "@playwright/test";
 
-//
-// Vérification du contrôle des notifications pour les utilisateurs.
-//
 test( "Vérification du contrôle des notifications", async ( { page } ) =>
 {
-	// Déclaration du test comme étant lent/instable.
 	test.slow();
 
-	// Réinitialisation des comptes utilisateurs factices.
 	execSync( "node scripts/create-fake-accounts.js" );
 
-	// Authentification et accès au tableau de bord.
 	await page.goto( "/authentication" );
 	await page.getByRole( "tab", { name: "Login" } ).click();
 	await page.getByPlaceholder( "name@domain.com" ).fill( "test1@gmail.com" );
 	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4016" );
 	await page.getByText( "Log in by password" ).click();
+
 	await expect( page ).toHaveURL( "/dashboard" );
 
-	// Téléversement de deux fichiers quelconques.
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
 	await page
 		.locator( "input[type = file]" )
-		.setInputFiles( [
-			join( __dirname, "static/cat.jpg" ),
-			join( __dirname, "static/duck.jpg" )
-		] );
+		.setInputFiles( [ join( __dirname, "static/cat.jpg" ), join( __dirname, "static/duck.jpg" ) ] );
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Ajout d'un utilisateur en partage en écriture sur le
-	//  premier fichier téléversé.
-	await page
-		.getByRole( "button", { name: "Open action menu" } )
-		.first()
-		.click();
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
+	await page.getByRole( "button", { name: "Open action menu" } ).first().click();
 	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
 	await page.getByPlaceholder( "Search..." ).fill( "test2@gmail.com" );
-	await page
-		.locator( "li" )
-		.filter( { hasText: "test2@gmail.com" } )
-		.getByRole( "button" )
-		.click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
+	await page.locator( "li" ).filter( { hasText: "test2@gmail.com" } ).getByRole( "button" ).click();
+
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
 	await page.getByRole( "combobox" ).click();
 	await page.getByLabel( "Write" ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 2 );
 
-	// Rechargement de la page pour fermer le menu des actions.
-	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
-	await page.reload();
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 2 );
 
-	// Ajout d'un utilisateur en partage en écriture sur le
-	//  deuxième fichier téléversé.
+	await page.reload(); // https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
 	await page.getByRole( "button", { name: "Open action menu" } ).last().click();
 	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
 	await page.getByPlaceholder( "Search..." ).fill( "test2@gmail.com" );
-	await page
-		.locator( "li" )
-		.filter( { hasText: "test2@gmail.com" } )
-		.getByRole( "button" )
-		.click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
+	await page.locator( "li" ).filter( { hasText: "test2@gmail.com" } ).getByRole( "button" ).click();
+
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
 	await page.getByRole( "combobox" ).click();
 	await page.getByLabel( "Write" ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 2 );
 
-	// Rechargement de la page pour fermer le menu des actions.
-	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
-	await page.reload();
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 2 );
 
-	// Déconnexion du compte utilisateur actuel.
+	await page.reload(); // https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
 	await page.locator( "header aside button:last-of-type" ).click();
 	await page.getByText( "Logout" ).click();
+
 	await expect( page ).toHaveURL( "/" );
 
-	// Connexion avec le compte utilisateur partagé en écriture.
 	await page.goto( "/authentication" );
 	await page.getByRole( "tab", { name: "Login" } ).click();
 	await page.getByPlaceholder( "name@domain.com" ).fill( "test2@gmail.com" );
 	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4016" );
 	await page.getByText( "Log in by password" ).click();
+
 	await expect( page ).toHaveURL( "/dashboard" );
 
-	// Réception des notifications et marquage de toutes comme lues.
 	await page.locator( ".lucide-bell-ring" ).click();
-	await expect( page.getByText( "Folder Sharing" ) ).toHaveCount( 2 );
-	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Suppression du partage en écriture du premier fichier partagé.
+	await expect( page.getByText( "Folder Sharing" ) ).toHaveCount( 2 );
+
+	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
+
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
 	await page.goto( "/dashboard" );
-	await page
-		.getByRole( "button", { name: "Open action menu" } )
-		.first()
-		.click();
+	await page.getByRole( "button", { name: "Open action menu" } ).first().click();
 	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
 	await page.getByRole( "button", { name: "Permanently Delete" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Rechargement de la page pour fermer le menu des actions.
-	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
-	await page.reload();
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
 
-	// Réception des notifications et vérification de la notification
-	//  de suppression du partage.
+	await page.reload(); // https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
+
 	await expect( page ).toHaveURL( "/dashboard" );
-	await page.locator( ".lucide-bell-ring" ).click();
-	await expect( page.getByText( "Deletion of a Shared File" ) ).toHaveCount( 1 );
-	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Mise à jour des préférences de notification (désactivation totale).
+	await page.locator( ".lucide-bell-ring" ).click();
+
+	await expect( page.getByText( "Deletion of a Shared File" ) ).toHaveCount( 1 );
+
+	await page.getByRole( "button", { name: "Mark All as Read" } ).click();
+
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
 	await page.goto( "/settings/notifications" );
 	await page.getByRole( "switch" ).last().click();
 	await page.getByRole( "button", { name: "Update" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Suppression du partage en écriture du deuxième fichier partagé.
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+
 	await page.goto( "/dashboard" );
 	await page.getByRole( "button", { name: "Open action menu" } ).click();
 	await page.getByRole( "menuitem", { name: "Manage Shares" } ).click();
 	await page.getByRole( "button", { name: "Permanently Delete" } ).click();
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
 
-	// Rechargement de la page pour fermer le menu des actions.
-	//  Source : https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
-	await page.reload();
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
 
-	// Attente de la réception des notifications et vérification des notifications.
+	await page.reload(); // https://github.com/radix-ui/primitives/issues/1836#issuecomment-1674338372
 	await page.locator( ".lucide-bell-ring" ).click();
+
 	await expect( page.getByText( "Deletion of a Shared File" ) ).toHaveCount( 0 );
 } );
--- tests/e2e/uploading.spec.ts
+++ tests/e2e/uploading.spec.ts
@@ -2,164 +2,75 @@ import { join } from "path";
 import { execSync } from "child_process";
 import { test, expect } from "@playwright/test";
 
-//
-// Authentification et accès au tableau de bord avant chaque test.
-//
 test.beforeEach( async ( { page } ) =>
 {
-	// Réinitialisation des comptes utilisateurs factices.
 	execSync( "node scripts/create-fake-accounts.js" );
 
-	// Accès à la page d'authentification.
 	await page.goto( "/authentication" );
-
-	// Clic sur l'onglet « Connexion ».
 	await page.getByRole( "tab", { name: "Login" } ).click();
-
-	// Remplissage des champs de saisie de l'adresse électronique et du mot de passe.
 	await page.getByPlaceholder( "name@domain.com" ).fill( "test1@gmail.com" );
 	await page.getByPlaceholder( "@MyPassword123!" ).fill( "Florian4016" );
-
-	// Clic sur le bouton de connexion.
 	await page.getByText( "Log in by password" ).click();
 
-	// Attente de la redirection vers la page du tableau de bord.
 	await expect( page ).toHaveURL( "/dashboard" );
 } );
 
-//
-// Téléversement d'un premier fichier valide.
-//
 test( "Téléversement d'un fichier valide", async ( { page } ) =>
 {
-	// Ouverture de la fenêtre de dialogue pour ajouter un fichier.
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
-
-	// Ajout d'une image quelconque.
-	await page
-		.locator( "input[type = file]" )
-		.setInputFiles( join( __dirname, "static/raccoon.jpg" ) );
-
-	// Clic sur le bouton de téléversement.
+	await page.locator( "input[type = file]" ).setInputFiles( join( __dirname, "static/raccoon.jpg" ) );
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
 
-	// Attente de la réponse du serveur et de la notification de succès.
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
 } );
 
-//
-// Téléversement d'un deuxième fichier valide et public.
-//
 test( "Téléversement d'un fichier public par défaut", async ( { page } ) =>
 {
-	// Accès aux paramètres utilisateur concernant le stockage.
 	await page.goto( "/settings/storage" );
-
-	// Activation de la publication automatique des fichiers téléversés.
-	await page
-		.getByLabel(
-			"Enable automatic publication of uploaded files to the server"
-		)
-		.click();
+	await page.getByLabel( "Enable automatic publication of uploaded files to the server" ).click();
 	await page.getByRole( "button", { name: "Update" } ).click();
 
-	// Retour à la page du tableau de bord et ouverture de la fenêtre de dialogue
-	//  pour ajouter un fichier.
 	await page.goto( "/dashboard" );
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
-
-	// Ajout d'une image quelconque.
-	await page
-		.locator( "input[type = file]" )
-		.setInputFiles( join( __dirname, "static/cat.jpg" ) );
-
-	// Clic sur le bouton de téléversement.
+	await page.locator( "input[type = file]" ).setInputFiles( join( __dirname, "static/cat.jpg" ) );
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
 
-	// Attente de la réponse du serveur et de la notification de succès.
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
-
-	// Vérification de la visibilité du fichier téléversé.
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
 	await expect( page.getByText( "public" ) ).toHaveCount( 1 );
 } );
 
-//
-// Téléversement d'un troisième fichier vide (sans contenu).
-//
 test( "Téléversement d'un fichier vide", async ( { page } ) =>
 {
-	// Téléversement d'un fichier vide.
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
-	await page
-		.locator( "input[type = file]" )
-		.setInputFiles( join( __dirname, "static/empty.txt" ) );
+	await page.locator( "input[type = file]" ).setInputFiles( join( __dirname, "static/empty.txt" ) );
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
 
-	// Attente de la réponse du serveur et de la notification d'erreur.
-	//  Note : la réponse sera négative car le fichier est vide.
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = error]" )
-	).toHaveCount( 1 );
+	await expect( page.locator( "[data-sonner-toast][data-type = error]" ) ).toHaveCount( 1 );
 } );
 
-//
-// Téléversement d'un quatrième fichier avec compression côte serveur.
-//
 test( "Téléversement d'un fichier compressé", async ( { page } ) =>
 {
-	// Téléversement d'un fichier quelconque.
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
-	await page
-		.locator( "input[type = file]" )
-		.setInputFiles( join( __dirname, "static/seagull.png" ) );
-
-	// Affichage des paramètres de téléversement avancés.
+	await page.locator( "input[type = file]" ).setInputFiles( join( __dirname, "static/seagull.png" ) );
 	await page.getByText( "Click here to display advanced settings." ).click();
-
-	// Activation de la compression des images et téléversement du fichier.
 	await page.getByLabel( "Enable image compression" ).click();
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
 
-	// Attente de la réponse du serveur et de la notification de succès.
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
 
-	// Fermeture de la boite de dialogue et vérification de la taille du fichier.
-	//  Note : la taille du fichier original est d'environ 433 KB.
 	await page.getByLabel( "Close toast" ).click();
+
 	await expect( page.getByRole( "cell", { name: "433.35 KB" } ) ).toHaveCount( 1 );
 } );
 
-//
-// Téléversement d'un cinquième fichier avec chiffrement côte client.
-//
 test( "Téléversement d'un fichier chiffré", async ( { page } ) =>
 {
-	// Téléversement d'un fichier quelconque et ouverture des paramètres avancés.
 	await page.locator( "button" ).filter( { hasText: "Add a file" } ).click();
-	await page
-		.locator( "input[type = file]" )
-		.setInputFiles( join( __dirname, "static/fox.jpg" ) );
+	await page.locator( "input[type = file]" ).setInputFiles( join( __dirname, "static/fox.jpg" ) );
 	await page.getByText( "Click here to display advanced settings." ).click();
-
-	// Activation du chiffrement des fichiers dans le navigateur avant
-	//  un quelconque téléversement au serveur.
 	await page.getByLabel( "Enable enhanced encryption" ).click();
 	await page.locator( "button" ).filter( { hasText: "Upload" } ).click();
 
-	// Attente de la réponse du serveur et de la notification de succès.
-	await expect(
-		page.locator( "[data-sonner-toast][data-type = success]" )
-	).toHaveCount( 1 );
-
-	// Attente de l'ouverture d'une boite de dialogue contenant une clé
-	//  de déchiffrement pour le fichier téléversé.
-	await expect(
-		page.getByRole( "button", { name: "Copy to Clipboard" } )
-	).toHaveCount( 1 );
+	await expect( page.locator( "[data-sonner-toast][data-type = success]" ) ).toHaveCount( 1 );
+	await expect( page.getByRole( "button", { name: "Copy to Clipboard" } ) ).toHaveCount( 1 );
 } );
